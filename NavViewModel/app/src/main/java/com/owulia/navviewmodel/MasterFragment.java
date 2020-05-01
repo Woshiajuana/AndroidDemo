b7e0c78f229f748248f0bb0c7154803b4579f9cb
Woshiajuana <zhigang.chen@owulia.com>
2020-05-01 22:32:19 +0800

    subcode code

--- NavViewModel/app/src/main/java/com/owulia/navviewmodel/MasterFragment.java
+++ NavViewModel/app/src/main/java/com/owulia/navviewmodel/MasterFragment.java
@@ -3,11 +3,17 @@ package com.owulia.navviewmodel;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.owulia.navviewmodel.databinding.FragmentMasterBinding;
 
 
 /**
@@ -22,10 +28,23 @@ public class MasterFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
+        MyViewModel myViewModel = new ViewModelProvider(getActivity(), new ViewModelProvider.NewInstanceFactory())
+                .get(MyViewModel.class);
+        FragmentMasterBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_master, container, false);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(getActivity());
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController controller = Navigation.findNavController(view);
+                controller.navigate(R.id.action_masterFragment_to_detailFragment);
+            }
+        });
+        return binding.getRoot();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_master, container, false);
+//        return inflater.inflate(R.layout.fragment_master, container, false);
     }
 
 }
