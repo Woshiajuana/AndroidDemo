37c0ca133aed5a0427c8727007eb20e6b3b9daf3
Woshiajuana <zhigang.chen@owulia.com>
2020-05-02 16:01:55 +0800

    subcode code

--- CalculationTest/app/src/main/java/com/owulia/calculationtest/QuestionFragment.java
+++ CalculationTest/app/src/main/java/com/owulia/calculationtest/QuestionFragment.java
@@ -3,11 +3,18 @@ package com.owulia.calculationtest;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.owulia.calculationtest.databinding.FragmentQuestionBinding;
 
 
 /**
@@ -22,10 +29,27 @@ public class QuestionFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        MyViewModel myViewModel = new ViewModelProvider(requireActivity(), new ViewModelProvider.NewInstanceFactory())
+                .get(MyViewModel.class);
+        FragmentQuestionBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_question, container, false);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(requireActivity());
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        };
+
+        binding.button0.setOnClickListener(listener);
+
+        return binding.getRoot();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_question, container, false);
+//        return inflater.inflate(R.layout.fragment_question, container, false);
     }
 
 }
--- CalculationTest/app/src/main/java/com/owulia/calculationtest/TitleFragment.java
+++ CalculationTest/app/src/main/java/com/owulia/calculationtest/TitleFragment.java
@@ -3,11 +3,18 @@ package com.owulia.calculationtest;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.owulia.calculationtest.databinding.FragmentTitleBinding;
 
 
 /**
@@ -22,10 +29,25 @@ public class TitleFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
+        MyViewModel myViewModel = new ViewModelProvider(requireActivity(), new ViewModelProvider.NewInstanceFactory())
+                .get(MyViewModel.class);
+        FragmentTitleBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(requireActivity());
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController controller = Navigation.findNavController(view);
+                controller.navigate(R.id.action_titleFragment_to_questionFragment);
+            }
+        });
+
+        return binding.getRoot();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false);
+//        return inflater.inflate(R.layout.fragment_title, container, false);
     }
 
 }
