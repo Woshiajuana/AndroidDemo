68630ffd10e3e9624abc37306c9ff60318719b18
Woshiajuana <zhigang.chen@owulia.com>
2020-05-03 11:49:20 +0800

    subcode code

--- RoomBasic/app/src/main/java/com/owulia/roombasic/MainActivity.java
+++ RoomBasic/app/src/main/java/com/owulia/roombasic/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -14,10 +16,12 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
     Button buttonInsert, buttonUpdate, buttonClear, buttonDelete;
 
     WordViewModel wordViewModel;
+
+    RecyclerView recyclerView;
+    MyAdapter myAdapter;
 
 
 
@@ -26,22 +30,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        myAdapter = new MyAdapter();
+        recyclerView.setAdapter(myAdapter);
+
 //        wordViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(WordViewModel.class);
         wordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
 
         wordViewModel.getAllWordsLive().observe(this, new Observer<List<Word>>() {
             @Override
             public void onChanged(List<Word> words) {
-                StringBuilder text = new StringBuilder();
-                for (int i = 0; i < words.size(); i++) {
-                    Word word = words.get(i);
-                    text.append(word.getId()).append(":").append(word.getWord()).append("=").append(word.getChineseMeaning()).append("\n");
-                }
-                textView.setText(text.toString());
+                myAdapter.setAllWords(words);
+                myAdapter.notifyDataSetChanged();
             }
         });
 
-        textView = findViewById(R.id.textView);
         buttonInsert = findViewById(R.id.buttonInsert);
         buttonInsert.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ RoomBasic/app/src/main/java/com/owulia/roombasic/MyAdapter.java
@@ -0,0 +1,54 @@
+package com.owulia.roombasic;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    List<Word> allWords = new ArrayList<>();
+
+    public void setAllWords(List<Word> allWords) {
+        this.allWords = allWords;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View itemView = layoutInflater.inflate(R.layout.cell_normal, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Word word = allWords.get(position);
+        holder.textViewNumber.setText(String.valueOf(position + 1));
+        holder.textViewEnglish.setText(word.getWord());
+        holder.textViewChinse.setText(word.getChineseMeaning());
+    }
+
+    @Override
+    public int getItemCount() {
+        return allWords.size();
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewNumber, textViewEnglish, textViewChinse;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewNumber = itemView.findViewById(R.id.textViewNumber);
+            textViewEnglish = itemView.findViewById(R.id.textViewEnglish);
+            textViewChinse = itemView.findViewById(R.id.textViewChinse);
+        }
+    }
+
+
+}
