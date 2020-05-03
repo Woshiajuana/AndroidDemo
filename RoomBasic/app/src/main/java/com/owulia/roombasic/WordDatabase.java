e71a12cd4d8dfcb300dd6762c7d7a0aa117fbffb
Woshiajuana <zhigang.chen@owulia.com>
2020-05-03 19:18:08 +0800

    subcode code

--- RoomBasic/app/src/main/java/com/owulia/roombasic/WordDatabase.java
+++ RoomBasic/app/src/main/java/com/owulia/roombasic/WordDatabase.java
@@ -2,9 +2,12 @@ package com.owulia.roombasic;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 // singleton
 @Database(entities = {Word.class}, version = 1, exportSchema = false)
@@ -16,10 +19,19 @@ public abstract class WordDatabase extends RoomDatabase {
     static synchronized WordDatabase getDatabase (Context context) {
         if (INSTANCE == null) {
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(), WordDatabase.class, "word_database")
-//                    .allowMainThreadQueries()
+//                    .allowMainThreadQueries() // 强制运行在主线程
+//                    .fallbackToDestructiveMigration() // 破坏性迁移，清除数据库从新建立数据结构
+                    .addMigrations()
                     .build();
         }
         return INSTANCE;
     }
+
+    static final Migration MIGRATION_2_3 = new Migration(1, 2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+//            database.execSQL();
+        }
+    };
 
 }
