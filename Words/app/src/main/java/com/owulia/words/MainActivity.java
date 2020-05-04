aeef1c70ec4d46cadb71127adbb9442eb499cb22
Woshiajuana <zhigang.chen@owulia.com>
2020-05-04 17:11:40 +0800

    subcode code

--- Words/app/src/main/java/com/owulia/words/MainActivity.java
+++ Words/app/src/main/java/com/owulia/words/MainActivity.java
@@ -1,109 +1,25 @@
 package com.owulia.words;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-
-import java.util.List;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonInsert, buttonUpdate, buttonClear, buttonDelete;
+    NavController navController;
 
-    WordViewModel wordViewModel;
-
-    RecyclerView recyclerView;
-    MyAdapter myAdapter1, myAdapter2;
-
-    Switch aSwitch;
-
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        wordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
-
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        myAdapter1 = new MyAdapter(false, wordViewModel);
-        myAdapter2 = new MyAdapter(true, wordViewModel);
-        recyclerView.setAdapter(myAdapter1);
-
-        aSwitch = findViewById(R.id.switch1);
-        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (b) {
-                    recyclerView.setAdapter(myAdapter2);
-                } else {
-                    recyclerView.setAdapter(myAdapter1);
-                }
-            }
-        });
-
-//        wordViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(WordViewModel.class);
-
-        wordViewModel.getAllWordsLive().observe(this, new Observer<List<Word>>() {
-            @Override
-            public void onChanged(List<Word> words) {
-                int temp = myAdapter1.getItemCount();
-                myAdapter1.setAllWords(words);
-                myAdapter2.setAllWords(words);
-                if (temp != words.size()) {
-                    myAdapter1.notifyDataSetChanged();
-                    myAdapter2.notifyDataSetChanged();
-                }
-            }
-        });
-
-        buttonInsert = findViewById(R.id.buttonInsert);
-        buttonInsert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Word word1 = new Word("Hello", "你好");
-                Word word2 = new Word("World", "世界");
-                wordViewModel.insertWords(word1, word2);
-            }
-        });
-
-        buttonUpdate = findViewById(R.id.buttonUpdate);
-        buttonUpdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Word word = new Word("Hi", "你好呀");
-                word.setId(20);
-                wordViewModel.updateWords(word);
-            }
-        });
-
-        buttonClear = findViewById(R.id.buttonClear);
-        buttonClear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wordViewModel.deleteAllWords();
-            }
-        });
-
-        buttonDelete = findViewById(R.id.buttonDelete);
-        buttonDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Word word = new Word("Hi", "你好呀");
-                word.setId(17);
-                wordViewModel.deleteWords(word);
-            }
-        });
+        navController = Navigation.findNavController(findViewById(R.id.fragment));
+        NavigationUI.setupActionBarWithNavController(this, navController);
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        navController.navigateUp();
+        return super.onSupportNavigateUp();
+    }
 }
--- Words/app/src/main/java/com/owulia/words/WordsFragment.java
+++ Words/app/src/main/java/com/owulia/words/WordsFragment.java
@@ -3,17 +3,33 @@ package com.owulia.words;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WordsFragment extends Fragment {
+
+    private WordViewModel wordViewModel;
+    private RecyclerView recyclerView;
+    private MyAdapter myAdapter1, myAdapter2;
+    private FloatingActionButton floatingActionButton;
 
 
     public WordsFragment() {
@@ -28,4 +44,34 @@ public class WordsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_words, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        wordViewModel = ViewModelProviders.of(requireActivity()).get(WordViewModel.class);
+        recyclerView = requireActivity().findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
+        myAdapter1 = new MyAdapter(false, wordViewModel);
+        myAdapter2 = new MyAdapter(true, wordViewModel);
+        recyclerView.setAdapter(myAdapter1);
+        wordViewModel.getAllWordsLive().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(List<Word> words) {
+                int temp = myAdapter1.getItemCount();
+                myAdapter1.setAllWords(words);
+                myAdapter2.setAllWords(words);
+                if (temp != words.size()) {
+                    myAdapter1.notifyDataSetChanged();
+                    myAdapter2.notifyDataSetChanged();
+                }
+            }
+        });
+        floatingActionButton = requireActivity().findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController navController = Navigation.findNavController(view);
+                navController.navigate(R.id.action_wordsFragment_to_addFragment);
+            }
+        });
+    }
 }
