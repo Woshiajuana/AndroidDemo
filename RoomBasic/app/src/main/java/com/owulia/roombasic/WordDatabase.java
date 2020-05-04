3799b1458bb1f03c2cd0e0e1ae8edd2d37875065
Woshiajuana <zhigang.chen@owulia.com>
2020-05-04 15:22:35 +0800

    subcode code

--- RoomBasic/app/src/main/java/com/owulia/roombasic/WordDatabase.java
+++ RoomBasic/app/src/main/java/com/owulia/roombasic/WordDatabase.java
@@ -21,16 +21,35 @@ public abstract class WordDatabase extends RoomDatabase {
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(), WordDatabase.class, "word_database")
 //                    .allowMainThreadQueries() // 强制运行在主线程
 //                    .fallbackToDestructiveMigration() // 破坏性迁移，清除数据库从新建立数据结构
-                    .addMigrations()
+//                    .addMigrations(MIGRATION_1_2) // 版本迁移策略
                     .build();
         }
         return INSTANCE;
     }
 
+    // 新增字段策略
     static final Migration MIGRATION_1_2 = new Migration(1, 2) {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase database) {
-            database.execSQL();
+            database.execSQL("ALTER TABLE word ADD COLUMN bar_data INTEGER NOT NULL DEFAULT 1");
+        }
+    };
+
+    // 删除字段策略
+    static final Migration MIGRATION_2_3 = new Migration(2, 3) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            // 创建一个新的
+            database.execSQL("CREATE TABLE word_temp (id INTEGER PRIMARY KEY NOT NULL, english_word TEXT," +
+                    "chinese_meaning TEXT)");
+            // 插入数据
+            database.execSQL("INSERT INTO word_temp (id, english_word, chinest_meaning)" +
+                    "SELECT id, english_word, chinest_meaning FROM word");
+            // 删除数据
+            database.execSQL("DROP TABLE word");
+            // 修改表
+            database.execSQL("ALTER TABLE word_temp RENAME TO word");
+
         }
     };
 
