493912b029eb32352e867fc89f92aed3dac932c1
Woshiajuana <zhigang.chen@owulia.com>
2020-05-05 15:10:28 +0800

    subcode code

---
+++ BottomNavigationDemo/app/src/main/java/com/owulia/bottomnavigationdemo/FirstViewModel.java
@@ -0,0 +1,7 @@
+package com.owulia.bottomnavigationdemo;
+
+import androidx.lifecycle.ViewModel;
+
+public class FirstViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ BottomNavigationDemo/app/src/main/java/com/owulia/bottomnavigationdemo/SecondViewModel.java
@@ -0,0 +1,7 @@
+package com.owulia.bottomnavigationdemo;
+
+import androidx.lifecycle.ViewModel;
+
+public class SecondViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
