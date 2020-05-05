5e49351c18992a20a0082acbd5e5f435c72b1d1e
Woshiajuana <zhigang.chen@owulia.com>
2020-05-05 19:34:58 +0800

    subcode code

--- PagingDemo/app/src/main/java/com/owulia/pagingdemo/MainActivity.java
+++ PagingDemo/app/src/main/java/com/owulia/pagingdemo/MainActivity.java
@@ -1,14 +1,25 @@
 package com.owulia.pagingdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    Button buttonPopulate, buttonClear;
+    StudentDao studentDao;
+    StudentDatabase studentDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+
     }
 }
