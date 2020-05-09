829bad1261d397ea2c1bdf9d08e41a2a548e46bc
Woshiajuana <zhigang.chen@owulia.com>
2020-05-09 14:00:22 +0800

    subcode code

--- Gallery/app/src/main/java/com/owulia/gallery/GalleryFragment.kt
+++ Gallery/app/src/main/java/com/owulia/gallery/GalleryFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.fragment_gallery.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +21,22 @@ class GalleryFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_gallery, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val galleryAdapter = GalleryAdapter()
+        recyclerView.apply {
+            adapter = galleryAdapter
+            layoutManager = GridLayoutManager(requireContext(), 2)
+        }
+
+        val galleryViewModel: GalleryViewModel = ViewModelProvider(this).get(GalleryViewModel::class.java)
+        galleryViewModel.photoListLive.observe(this, Observer {
+            galleryAdapter.submitList(it)
+        })
+
+        galleryViewModel.photoListLive.value?:galleryViewModel.fetchData()
     }
 
 }
