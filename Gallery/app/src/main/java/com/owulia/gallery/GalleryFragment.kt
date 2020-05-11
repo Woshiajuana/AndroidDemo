e0ce873985a066250e2a4a49342f6cef815e083a
Woshiajuana <zhigang.chen@owulia.com>
2020-05-11 21:39:27 +0800

    subcode code

--- Gallery/app/src/main/java/com/owulia/gallery/GalleryFragment.kt
+++ Gallery/app/src/main/java/com/owulia/gallery/GalleryFragment.kt
@@ -1,10 +1,8 @@
 package com.owulia.gallery
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
@@ -15,6 +13,8 @@ import kotlinx.android.synthetic.main.fragment_gallery.*
  */
 class GalleryFragment : Fragment() {
 
+    private lateinit var galleryViewModel: GalleryViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,15 +23,28 @@ class GalleryFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_gallery, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.swipeIndictor -> galleryViewModel.fetchData()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setHasOptionsMenu(true)
         val galleryAdapter = GalleryAdapter()
         recyclerView.apply {
             adapter = galleryAdapter
             layoutManager = GridLayoutManager(requireContext(), 2)
         }
 
-        val galleryViewModel: GalleryViewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(requireActivity().application)).get(GalleryViewModel::class.java)
+        galleryViewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(requireActivity().application)).get(GalleryViewModel::class.java)
         galleryViewModel.photoListLive.observe(this, Observer {
             galleryAdapter.submitList(it)
             swipeLayoutGallery.isRefreshing = false
