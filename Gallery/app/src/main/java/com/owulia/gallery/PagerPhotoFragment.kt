0f0b1ec888663a0cfa472fff602dfd45406f3418
Woshiajuana <zhigang.chen@owulia.com>
2020-05-12 21:58:08 +0800

    subcode code

--- Gallery/app/src/main/java/com/owulia/gallery/PagerPhotoFragment.kt
+++ Gallery/app/src/main/java/com/owulia/gallery/PagerPhotoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_pager_photo.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +18,13 @@ class PagerPhotoFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pager_photo, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        PagerPhotoListAdapter().apply {
+            viewPager2.adapter = this
+        }
     }
 
 }
