4741dfe6409b582ed3264bd2cc0676652bdec644
Woshiajuana <zhigang.chen@owulia.com>
2020-05-13 14:17:23 +0800

    subcode code

--- Gallery/app/src/main/java/com/owulia/gallery/PagerPhotoFragment.kt
+++ Gallery/app/src/main/java/com/owulia/gallery/PagerPhotoFragment.kt
@@ -1,17 +1,29 @@
 package com.owulia.gallery
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.fragment_pager_photo.*
+import kotlinx.android.synthetic.main.pager_photo_view.view.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * A simple [Fragment] subclass.
@@ -44,14 +56,22 @@ class PagerPhotoFragment : Fragment() {
             }
         })
 
-        viewPager2.setCurrentItem(arguments?.getInt("PHOTO_POSITION")?:0, false)
+        viewPager2.setCurrentItem(arguments?.getInt("PHOTO_POSITION") ?: 0, false)
 
         saveButton.setOnClickListener {
-            if (Build.VERSION.SDK_INT < 29 && ContextCompat.checkSelfPermission(requireContext(),
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_WRITE_EXTERNAL_STORAGE)
+            if (Build.VERSION.SDK_INT < 29 && ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions(
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    REQUEST_WRITE_EXTERNAL_STORAGE
+                )
             } else {
-                savePhoto()
+                viewLifecycleOwner.lifecycleScope.launch {
+                    savePhoto()
+                }
             }
         }
 
@@ -66,7 +86,9 @@ class PagerPhotoFragment : Fragment() {
         when (requestCode) {
             REQUEST_WRITE_EXTERNAL_STORAGE -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    savePhoto()
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        savePhoto()
+                    }
                 } else {
                     Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
                 }
@@ -74,8 +96,44 @@ class PagerPhotoFragment : Fragment() {
         }
     }
 
-    private fun savePhoto () {
-        Toast.makeText(requireContext(), "存储成功", Toast.LENGTH_SHORT).show()
+    private suspend fun savePhoto() {
+
+        withContext(Dispatchers.IO) {
+
+            val holder =
+                (viewPager2[0] as RecyclerView).findViewHolderForAdapterPosition(viewPager2.currentItem)
+                        as PagerPhotoViewHolder
+            val bitmap = holder.itemView.pagerPhoto.drawable.toBitmap()
+//        // SDK29以下可以这么操作
+//        if (MediaStore.Images.Media.insertImage(requireContext().contentResolver, bitmap, "", "") == null) {
+//            Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
+//        } else {
+//            Toast.makeText(requireContext(), "存储成功", Toast.LENGTH_SHORT).show()
+//        }
+
+            val saveUri = requireContext().contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                ContentValues()
+            )?: kotlin.run {
+                MainScope().launch {
+                    Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
+                }
+                return@withContext
+            }
+            requireContext().contentResolver.openOutputStream(saveUri).use {
+                if (bitmap.compress(Bitmap.CompressFormat.JPEG, 90, it)) {
+                    MainScope().launch {
+                        Toast.makeText(requireContext(), "存储成功", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    MainScope().launch {
+                        Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+        }
+
+
     }
 
 }
