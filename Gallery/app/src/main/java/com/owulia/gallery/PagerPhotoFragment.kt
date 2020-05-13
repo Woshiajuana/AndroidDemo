5d421d678b5ab913b6e7a34c35c058bb409e00de
Woshiajuana <zhigang.chen@owulia.com>
2020-05-13 11:47:31 +0800

    subcode code

--- Gallery/app/src/main/java/com/owulia/gallery/PagerPhotoFragment.kt
+++ Gallery/app/src/main/java/com/owulia/gallery/PagerPhotoFragment.kt
@@ -1,16 +1,24 @@
 package com.owulia.gallery
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.fragment_pager_photo.*
 
 /**
  * A simple [Fragment] subclass.
  */
+
+const val REQUEST_WRITE_EXTERNAL_STORAGE = 1
+
 class PagerPhotoFragment : Fragment() {
 
     override fun onCreateView(
@@ -38,6 +46,36 @@ class PagerPhotoFragment : Fragment() {
 
         viewPager2.setCurrentItem(arguments?.getInt("PHOTO_POSITION")?:0, false)
 
+        saveButton.setOnClickListener {
+            if (Build.VERSION.SDK_INT < 29 && ContextCompat.checkSelfPermission(requireContext(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_WRITE_EXTERNAL_STORAGE)
+            } else {
+                savePhoto()
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_WRITE_EXTERNAL_STORAGE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    savePhoto()
+                } else {
+                    Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun savePhoto () {
+        Toast.makeText(requireContext(), "存储成功", Toast.LENGTH_SHORT).show()
     }
 
 }
