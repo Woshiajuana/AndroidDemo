be654299810ac8be6cb3c73c3e6dc7e88425725c
Woshiajuana <zhigang.chen@owulia.com>
2020-05-15 15:22:25 +0800

    subcode code

--- MakeKotlin/app/src/main/java/com/owulia/makekotlin/MainActivity.kt
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/MainActivity.kt
@@ -2,14 +2,22 @@ package com.owulia.makekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.owulia.makekotlin.views.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private var numCurrent = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+
+        supportFragmentManager.beginTransaction().add(R.id.frameLayout, HomeFragment()).commit()
+    }
+
+    private fun init () {
 
     }
 
