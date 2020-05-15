110d3d97d49cc712404d394b9e83544330178875
Woshiajuana <zhigang.chen@owulia.com>
2020-05-15 16:00:11 +0800

    subcode code

--- MakeKotlin/app/src/main/java/com/owulia/makekotlin/MainActivity.kt
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/MainActivity.kt
@@ -2,23 +2,56 @@ package com.owulia.makekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.Fragment
 import com.owulia.makekotlin.views.HomeFragment
+import com.owulia.makekotlin.views.MaterialFragment
+import com.owulia.makekotlin.views.MineFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "MainActivity"
     private var numCurrent = 0
+
+    private lateinit var homeFragment: Fragment
+    private lateinit var materialFragment: Fragment
+    private lateinit var mineFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-        supportFragmentManager.beginTransaction().add(R.id.frameLayout, HomeFragment()).commit()
+        init()
+        supportFragmentManager.beginTransaction().add(R.id.frameLayout, homeFragment).commit()
     }
 
     private fun init () {
+        homeFragment = HomeFragment()
+        materialFragment = MaterialFragment()
+        mineFragment = MineFragment()
+        tabBarHome.setOnClickListener {
+            handleSwitch(it, homeFragment)
+        }
+        tabBarMaterial.setOnClickListener {
+            handleSwitch(it, materialFragment)
+        }
+        tabBarMine.setOnClickListener {
+            handleSwitch(it, mineFragment)
+        }
+    }
 
+    private fun handleSwitch (view: View, fragment: Fragment) {
+        when(view.id) {
+            R.id.tabBarHome -> {
+                tabBarHome
+            }
+            R.id.tabBarMaterial -> {
+            }
+            R.id.tabBarMine -> {
+            }
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit();
     }
 
 }
