3b28bd9fa49c4f6a538f480544b05cd00b95ced1
Woshiajuana <zhigang.chen@owulia.com>
2020-05-15 16:33:11 +0800

    subcode code

--- MakeKotlin/app/src/main/java/com/owulia/makekotlin/MainActivity.kt
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.owulia.makekotlin.views.HomeFragment
 import com.owulia.makekotlin.views.MaterialFragment
@@ -23,7 +24,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
-        supportFragmentManager.beginTransaction().add(R.id.frameLayout, homeFragment).commit()
     }
 
     private fun init () {
@@ -39,16 +39,30 @@ class MainActivity : AppCompatActivity() {
         tabBarMine.setOnClickListener {
             handleSwitch(it, mineFragment)
         }
+        handleSwitch(tabBarHome, homeFragment)
     }
 
     private fun handleSwitch (view: View, fragment: Fragment) {
+        tabBarHomeImage.setImageResource(R.mipmap.tab_bar_home_normal)
+        tabBarMaterialImage.setImageResource(R.mipmap.tab_bar_material_normal)
+        tabBarMineImage.setImageResource(R.mipmap.tab_bar_mine_normal)
+        val normalColor = ContextCompat.getColor(this,R.color.tabBarTextColorNormal)
+        val activeColor = ContextCompat.getColor(this,R.color.tabBarTextColorActive)
+        tabBarHomeText.setTextColor(normalColor)
+        tabBarMaterialText.setTextColor(normalColor)
+        tabBarMineText.setTextColor(normalColor)
         when(view.id) {
             R.id.tabBarHome -> {
-                tabBarHome
+                tabBarHomeImage.setImageResource(R.mipmap.tab_bar_home_active)
+                tabBarHomeText.setTextColor(activeColor)
             }
             R.id.tabBarMaterial -> {
+                tabBarMaterialImage.setImageResource(R.mipmap.tab_bar_material_active)
+                tabBarMaterialText.setTextColor(activeColor)
             }
             R.id.tabBarMine -> {
+                tabBarMineImage.setImageResource(R.mipmap.tab_bar_mine_active)
+                tabBarMineText.setTextColor(activeColor)
             }
         }
         supportFragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit();
