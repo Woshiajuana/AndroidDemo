12668f2b4db321baa1e60d764d9d50b95c5bd1bd
Woshiajuana <zhigang.chen@owulia.com>
2020-05-18 17:15:57 +0800

    subcode code

---
+++ ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/activitys/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.owulia.imoocmusicdemo.activitys
+
+import android.app.Activity
+import android.os.Bundle
+//import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/activitys/MainActivity.kt
+++ ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/activitys/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.owulia.imoocmusicdemo
+package com.owulia.imoocmusicdemo.activitys
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.owulia.imoocmusicdemo.R
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
