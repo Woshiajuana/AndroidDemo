07d9c785cf6479db2c15ccfc2f24a60d394078fc
Woshiajuana <zhigang.chen@owulia.com>
2020-05-18 18:03:57 +0800

    subcode code

--- ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/activitys/WelcomeActivity.kt
+++ ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/activitys/WelcomeActivity.kt
@@ -1,14 +1,40 @@
 package com.owulia.imoocmusicdemo.activitys
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.owulia.imoocmusicdemo.R
+import java.util.*
 
 // 延迟3秒
 // 跳转页面
 class WelcomeActivity : BaseActivity() {
 
+    private lateinit var mTimer : Timer
+    private val TAG = "WelcomeActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        init()
+    }
+
+    // 初始化
+    fun init () {
+        mTimer = Timer()
+        mTimer.schedule(object: TimerTask() {
+            override fun run() {
+                Log.d(TAG, "当前线程${Thread.currentThread()}")
+                toMain()
+            }
+        }, 3 * 1000)
+    }
+
+    // 跳转页面
+    fun toMain () {
+        var intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
