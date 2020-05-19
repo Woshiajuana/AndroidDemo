8b3ccf2425318c0773186e7197088a626cee9072
Woshiajuana <zhigang.chen@owulia.com>
2020-05-19 22:29:54 +0800

    subcode code

--- ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/utils/UserUtil.kt
+++ ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/utils/UserUtil.kt
@@ -1,8 +1,13 @@
 package com.owulia.imoocmusicdemo.utils
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.text.TextUtils
 import com.blankj.utilcode.util.RegexUtils
 import com.blankj.utilcode.util.ToastUtils
+import com.owulia.imoocmusicdemo.R
+import com.owulia.imoocmusicdemo.activitys.LoginActivity
 
 class UserUtil {
     companion object {
@@ -33,6 +38,13 @@ class UserUtil {
             }
 
             return true
+        }
+
+        fun logout (context: Context) {
+            val intent = Intent(context, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            context.startActivity(intent)
+            (context as Activity).overridePendingTransition(R.anim.open_enter, R.anim.open_exit)
         }
     }
 
