f5b6938ec7cd0dd87307c9dd488f5f033eb14845
Woshiajuana <zhigang.chen@owulia.com>
2020-05-20 11:59:51 +0800

    subcode code

--- ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/activitys/MainActivity.kt
+++ ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/activitys/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.owulia.imoocmusicdemo.activitys
 
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.owulia.imoocmusicdemo.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,11 @@ class MainActivity : BaseActivity() {
 
     private fun initView () {
         initNavBar(false,"码可音乐", true)
+
+        mMusicGird.apply {
+            adapter =
+            layoutManager = GridLayoutManager(context, 3)
+        }
+
     }
 }
---
+++ ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/adapters/MusicGridAdapter.kt
@@ -0,0 +1,14 @@
+package com.owulia.imoocmusicdemo.adapters
+
+import android.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class MusicGridAdapter : RecyclerView.Adapter<MusicGridHolder> {
+
+
+
+}
+
+class MusicGridHolder : RecyclerView.ViewHolder {
+
+}
