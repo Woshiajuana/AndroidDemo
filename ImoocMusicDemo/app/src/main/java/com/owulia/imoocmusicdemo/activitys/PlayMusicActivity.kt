b5cf44978ce6a258dbe5e51502e4e08f64e8e053
Woshiajuana <zhigang.chen@owulia.com>
2020-05-20 18:18:16 +0800

    subcode code

--- ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/activitys/PlayMusicActivity.kt
+++ ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/activitys/PlayMusicActivity.kt
@@ -3,7 +3,9 @@ package com.owulia.imoocmusicdemo.activitys
 import android.os.Bundle
 import android.view.View
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.owulia.imoocmusicdemo.R
+import jp.wasabeef.glide.transformations.BlurTransformation
 import kotlinx.android.synthetic.main.activity_play_music.*
 
 class PlayMusicActivity : BaseActivity() {
@@ -16,7 +18,7 @@ class PlayMusicActivity : BaseActivity() {
 
         Glide.with(this)
             .load("https://img4.mukewang.com/szimg/5d43953c09c0247612000676-228-128.png")
-//            .transition()
+            .apply(RequestOptions.bitmapTransform(BlurTransformation(25, 10)))
             .into(mMusicBg)
     }
 
