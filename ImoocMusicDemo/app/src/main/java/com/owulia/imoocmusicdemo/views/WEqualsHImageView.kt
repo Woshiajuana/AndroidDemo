d4c57a905409f5e3cf94e8ac49213edc7e352e11
Woshiajuana <zhigang.chen@owulia.com>
2020-05-20 14:35:38 +0800

    subcode code

--- ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/views/WEqualsHImageView.kt
+++ ImoocMusicDemo/app/src/main/java/com/owulia/imoocmusicdemo/views/WEqualsHImageView.kt
@@ -8,4 +8,12 @@ class WEqualsHImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : AppCompatImageView(context, attrs, defStyleAttr) {
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+//        // 获取 View 的宽度
+//        val width = MeasureSpec.getSize(widthMeasureSpec)
+//        // 获取 View 的模式 match_parent 、 wrap_parent 、具体的 dp
+//        val mode = MeasureSpec.getMode(widthMeasureSpec)
+    }
+
 }
