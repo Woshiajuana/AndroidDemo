e6dee83de42858c5abfacb236b6c28b758d58f1e
Woshiajuana <zhigang.chen@owulia.com>
2020-05-30 22:23:19 +0800

    subcode code

--- WowCool/app/src/main/java/com/owulia/wowcool/MainActivity.kt
+++ WowCool/app/src/main/java/com/owulia/wowcool/MainActivity.kt
@@ -2,11 +2,15 @@ package com.owulia.wowcool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        vWowTabBar.setItem(R.mipmap.tab_bar_home_normal, R.mipmap.tab_bar_home_active, "首页",  HomeFragment())
+
     }
 }
--- WowCool/app/src/main/java/com/owulia/wowcool/WowTabBar.kt
+++ WowCool/app/src/main/java/com/owulia/wowcool/WowTabBar.kt
@@ -3,47 +3,156 @@ package com.owulia.wowcool
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
+import android.view.Gravity
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
+import org.w3c.dom.Text
 
 class WowTabBar @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
-//    private var viewPager
+    private lateinit var viewInner: ViewPager
+    private lateinit var viewBottom: LinearLayout
+    private lateinit var viewDivider: View
 
     init {
 
-        // 初始化
-        val vViewPager = ViewPager(context)
+        renderWrapView()
 
+        renderInnerView()
+
+        renderDividerView()
+
+        renderBottomView()
+
+        render()
+
+    }
+
+    // 初始化容器布局
+    private fun renderWrapView() {
         orientation = VERTICAL
+    }
 
-//        var v = LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT)
-//        v.weight = 1f
-        (vViewPager.layoutParams as LayoutParams).apply {
-            weight = 1f
+    // 初始化 ViewPager
+    private fun renderInnerView() {
+        viewInner = ViewPager(context)
+        val params = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+            .apply {
+                weight = 1f
+            }
+        viewInner.apply {
+            layoutParams = params
+            setBackgroundColor(Color.parseColor("#ffff0000"))
         }
-        vViewPager.setBackgroundColor(Color.parseColor("#ffff0000"))
+    }
 
-        var vText = TextView(context)
-        vText.text = "你好呀"
+    // 初始化分割线
+    private fun renderDividerView() {
+        viewDivider = View(context)
+        val params = LayoutParams(LayoutParams.MATCH_PARENT, 1)
+        viewDivider.apply {
+            layoutParams = params
+            setBackgroundColor(Color.parseColor("#ff00ff00"))
+        }
+    }
 
+    // 初始化底部布局
+    private fun renderBottomView() {
+        viewBottom = LinearLayout(context)
+        val params = LayoutParams(LayoutParams.MATCH_PARENT, 100)
+        viewBottom.apply {
+            layoutParams = params
+            orientation = HORIZONTAL
+            setBackgroundColor(Color.parseColor("#ff0000ff"))
+        }
+    }
 
+    // 添加布局
+    private fun render() {
+        addView(viewInner)
+        addView(viewDivider)
+        addView(viewBottom)
+    }
 
-        addView(vViewPager)
-        addView(vText)
+    fun setItem (icon: Int, iconSelect: Int, text: String, fragment: Fragment) {
+        viewBottom.addView(WowTabBarItem(context))
+        viewBottom.addView(WowTabBarItem(context))
+        viewBottom.addView(WowTabBarItem(context))
+        viewBottom.addView(WowTabBarItem(context))
+    }
+
+    fun setColor (color: Color, colorSelect: Color) {
+
+    }
+
+    fun build () {
+
+    }
+
+}
+
+class WowTabBarItem @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    init {
+        render()
+
+        renderImage()
+
+        val text = TextView(context)
+        text.text = "h哈哈哈"
+        text.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+//            .apply {
+//                gravity = Gravity.CENTER_HORIZONTAL
+//            }
+        text.setBackgroundColor(Color.parseColor("#ffffffff"))
+//        addView(text)
 
 
     }
 
-    // 初始化 ViewPager
-    private fun renderViewPager () {
-        val layoutParams = LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT)
-        layoutParams.apply {
-            weight = 1f
+    private fun renderImage () {
+        val viewImageNormal = ImageView(context)
+        val viewImageActive = ImageView(context)
+        val paramsImage = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+        viewImageNormal.apply {
+            layoutParams = paramsImage
+            setImageResource(R.mipmap.tab_bar_home_normal)
         }
+        viewImageActive.apply {
+            layoutParams = paramsImage
+            setImageResource(R.mipmap.tab_bar_home_active)
+        }
+
+        val viewFragment = FrameLayout(context)
+        val paramsFragment = LayoutParams(48, 48) .apply {
+            gravity = Gravity.CENTER
+        }
+        viewFragment.apply {
+            layoutParams = paramsFragment
+            setBackgroundColor(Color.parseColor("#ffdddddd"))
+            addView(viewImageActive)
+            addView(viewImageNormal)
+        }
+
+        addView(viewFragment)
+    }
+
+    private fun render () {
+        orientation = VERTICAL
+        val params = LayoutParams(0, LayoutParams.MATCH_PARENT)
+        params.weight = 1f
+        gravity = Gravity.CENTER
+        layoutParams = params
+        setBackgroundColor(Color.parseColor("#ff333333"))
     }
 
 
