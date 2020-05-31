2076860db35737f37ebc0011dc86a716e5f6f78d
Woshiajuana <zhigang.chen@owulia.com>
2020-05-31 21:23:55 +0800

    subcode code

--- WowCool/app/src/main/java/com/owulia/wowcool/MainActivity.kt
+++ WowCool/app/src/main/java/com/owulia/wowcool/MainActivity.kt
@@ -13,7 +13,9 @@ class MainActivity : AppCompatActivity() {
         vWowTabBar.apply {
             setItemText("#ff000000", "#ff007FD6")
             addedItem(R.mipmap.tab_bar_home_normal, R.mipmap.tab_bar_home_active, "首页",  HomeFragment())
-            build()
+            addedItem(R.mipmap.tab_bar_material_normal, R.mipmap.tab_bar_material_active, "有料",  HomeFragment())
+            addedItem(R.mipmap.tab_bar_mine_normal, R.mipmap.tab_bar_mine_active, "我的",  HomeFragment())
+            build(supportFragmentManager)
         }
 
     }
--- WowCool/app/src/main/java/com/owulia/wowcool/WowTabBar.kt
+++ WowCool/app/src/main/java/com/owulia/wowcool/WowTabBar.kt
@@ -3,13 +3,17 @@ package com.owulia.wowcool
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.marginTop
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import org.w3c.dom.Text
 
@@ -21,14 +25,14 @@ class WowTabBar @JvmOverloads constructor(
     private lateinit var viewBottom: LinearLayout
     private lateinit var viewDivider: View
 
-    private var multiList = mutableListOf<WowTabBarItem>()
+    private var arrTabBarItem = mutableListOf<WowTabBarItem>()
 
     // 普通图片
-    private var arrNormalIcon = mutableListOf<Int>()
+    private var arrIconNormal = mutableListOf<Int>()
     // 选中图片
-    private var arrActiveIcon = mutableListOf<Int>()
+    private var arrIconActive = mutableListOf<Int>()
     // 文字
-    private var arrText = mutableListOf<String>()
+    private var arrTextItem = mutableListOf<String>()
     // Fragment
     private var arrFragment = mutableListOf<Fragment>()
 
@@ -80,7 +84,7 @@ class WowTabBar @JvmOverloads constructor(
     // 初始化底部布局
     private fun renderBottomView() {
         viewBottom = LinearLayout(context)
-        val params = LayoutParams(LayoutParams.MATCH_PARENT, 100)
+        val params = LayoutParams(LayoutParams.MATCH_PARENT, 150)
         viewBottom.apply {
             layoutParams = params
             orientation = HORIZONTAL
@@ -95,25 +99,65 @@ class WowTabBar @JvmOverloads constructor(
         addView(viewBottom)
     }
 
-    // 添加
+    // 添加wowTabBarItem
     fun addedItem (icon: Int, iconSelect: Int, text: String, fragment: Fragment) : WowTabBar {
-        arrNormalIcon.add(icon)
-        arrActiveIcon.add(iconSelect)
-        arrText.add(text)
+        arrIconNormal.add(icon)
+        arrIconActive.add(iconSelect)
+        arrTextItem.add(text)
         arrFragment.add(fragment)
         return this
     }
 
+    // 设置文字
     fun setItemText (color: String, colorSelect: String) : WowTabBar {
         colorNormalText = Color.parseColor(color)
         colorActiveText = Color.parseColor(colorSelect)
         return this
     }
 
-    fun build () {
-        multiList.forEach {
-            viewBottom.addView(it)
+    // 设置 adapter
+    private fun setViewPagerAdapter (fragmentManager: FragmentManager) {
+        viewInner.apply {
+            offscreenPageLimit = arrIconNormal.size
+            adapter = object: FragmentPagerAdapter(fragmentManager) {
+                override fun getItem(position: Int): Fragment {
+                    return arrFragment[position]
+                }
+                override fun getCount(): Int {
+                    return arrTextItem.size
+                }
+            }
+            addOnPageChangeListener(object: ViewPager.OnPageChangeListener{
+                override fun onPageScrollStateChanged(state: Int) {}
+                override fun onPageScrolled(
+                    position: Int,
+                    positionOffset: Float,
+                    positionOffsetPixels: Int
+                ) {
+                    if (positionOffset > 0) {
+                        
+                    }
+                }
+                override fun onPageSelected(position: Int) {}
+            })
         }
+    }
+
+    fun build (fragmentManager: FragmentManager) : WowTabBar {
+        arrIconNormal.forEachIndexed { index: Int, item: Int ->
+            val wowTabBarItem = WowTabBarItem(
+                context,
+                item,
+                arrIconActive[index],
+                arrTextItem[index],
+                colorNormalText,
+                colorActiveText
+            )
+            arrTabBarItem.add(wowTabBarItem)
+            viewBottom.addView(wowTabBarItem)
+        }
+        setViewPagerAdapter (fragmentManager)
+        return this
     }
 
 }
@@ -122,6 +166,20 @@ class WowTabBarItem @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
+    private var iconNormal: Int = 0
+    private var iconActive: Int = 0
+    private var textItem: String = ""
+    private var colorNormalText = Color.parseColor("#ff000000")
+    private var colorActiveText = Color.parseColor("#ff007FD6")
+
+    private lateinit var viewImageNormal: ImageView
+    private lateinit var viewImageActive: ImageView
+    private lateinit var viewText: TextView
+
+    init {
+        renderWrapView()
+    }
+
     constructor(
         context: Context,
         icon: Int,
@@ -129,32 +187,43 @@ class WowTabBarItem @JvmOverloads constructor(
         text: String,
         color: Int,
         colorSelect: Int
-    ) : this(context)
+    ) : this(context) {
+        iconNormal = icon
+        iconActive = iconSelect
+        textItem = text
+        colorNormalText = color
+        colorActiveText = colorSelect
 
-    init {
-        render()
+        renderImageView()
+        renderTextView()
 
-        renderImage()
-
-        renderText()
-
+        setProgress(0f)
     }
 
-    private fun renderImage () {
-        val viewImageNormal = ImageView(context)
-        val viewImageActive = ImageView(context)
+    private fun renderWrapView () {
+        val params = LayoutParams(0, LayoutParams.MATCH_PARENT).apply {
+            weight = 1f
+        }
+        layoutParams = params
+        orientation = VERTICAL
+        gravity = Gravity.CENTER
+    }
+
+    private fun renderImageView () {
+        viewImageNormal = ImageView(context)
+        viewImageActive = ImageView(context)
         val paramsImage = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
         viewImageNormal.apply {
             layoutParams = paramsImage
-            setImageResource(R.mipmap.tab_bar_home_normal)
+            setImageResource(iconNormal)
         }
         viewImageActive.apply {
             layoutParams = paramsImage
-            setImageResource(R.mipmap.tab_bar_home_active)
+            setImageResource(iconActive)
         }
 
         val viewFragment = FrameLayout(context)
-        val paramsFragment = LayoutParams(48, 48) .apply {
+        val paramsFragment = LayoutParams(60, 60) .apply {
             gravity = Gravity.CENTER
         }
         viewFragment.apply {
@@ -165,22 +234,41 @@ class WowTabBarItem @JvmOverloads constructor(
         addView(viewFragment)
     }
 
-    private fun renderText () {
-        val text = TextView(context)
-        text.text = "h哈哈哈"
-        text.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        text.textSize = 10f
-        addView(text)
-    }
-
-    private fun render () {
-        val params = LayoutParams(0, LayoutParams.MATCH_PARENT).apply {
-            weight = 1f
+    private fun renderTextView () {
+        viewText = TextView(context)
+        viewText.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+            .apply {
+                topMargin = 10
+            }
+        viewText.apply {
+            textSize = 10f
+            text = textItem
+            setTextColor(colorNormalText)
         }
-        layoutParams = params
-        orientation = VERTICAL
-        gravity = Gravity.CENTER
+        addView(viewText)
     }
 
+    fun setProgress (progress: Float) {
+        viewImageNormal.alpha = 1 - progress
+        viewImageActive.alpha = progress
+        viewText.setTextColor(evaluate(progress, colorNormalText, colorActiveText))
+    }
+
+    private fun evaluate (progress: Float, startValue: Int, endValue: Int) : Int {
+        val startA = startValue shr 24 and 0xff
+        val startR = startValue shr 16 and 0xff
+        val startG = startValue shr 8 and 0xff
+        val startB = startValue and 0xff
+
+        val endA = endValue shr 24 and 0xff
+        val endR = endValue shr 16 and 0xff
+        val endG = endValue shr 8 and 0xff
+        val endB = endValue and 0xff
+
+        return (startA + progress * (endA - startA)).toInt() shl 24 or (
+                (startR + progress * (endR - startR)).toInt() shl 16) or (
+                (startG + progress * (endG - startG)).toInt() shl 8) or
+                (startB + progress * (endB - startB)).toInt()
+    }
 
 }
