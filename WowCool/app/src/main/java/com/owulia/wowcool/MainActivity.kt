f6b6e9e51369102df5ee90316ff0562d38eb2aa4
Woshiajuana <zhigang.chen@owulia.com>
2020-06-01 11:24:34 +0800

    subcode code

--- WowCool/app/src/main/java/com/owulia/wowcool/MainActivity.kt
+++ WowCool/app/src/main/java/com/owulia/wowcool/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         vWowTabBar.apply {
             setItemText("#ff000000", "#ff007FD6")
             addedItem(R.mipmap.tab_bar_home_normal, R.mipmap.tab_bar_home_active, "首页",  HomeFragment())
-            addedItem(R.mipmap.tab_bar_material_normal, R.mipmap.tab_bar_material_active, "有料",  MaterialFragment())
+            addedItem(R.mipmap.tab_bar_demo_normal, R.mipmap.tab_bar_demo_active, "示例",  MaterialFragment())
             addedItem(R.mipmap.tab_bar_mine_normal, R.mipmap.tab_bar_mine_active, "我的",  MineFragment())
             build(supportFragmentManager)
             switchItem(0)
