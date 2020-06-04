2c8e8d140c9f13b984142ea44eac22aab3a52a98
Woshiajuana <zhigang.chen@owulia.com>
2020-06-04 21:57:35 +0800

    subcode code

---
+++ WowCool/app/src/main/java/com/owulia/wowcool/widget/WowIconFontView.kt
@@ -0,0 +1,22 @@
+package com.owulia.wowcool.widget
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class WowIconFontView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    private val iconFontTypeFace: Typeface by lazy {
+        Typeface.createFromAsset(context.assets, "fonts/iconfont.ttf")
+    }
+
+    init {
+        typeface = iconFontTypeFace
+        includeFontPadding = false
+    }
+
+}
+
