30cdb530c40553e7276e79f234a0341afec364dd
Woshiajuana <zhigang.chen@owulia.com>
2020-06-09 21:19:50 +0800

    subcode code

---
+++ WowCool/app/src/main/java/com/owulia/wowcool/adapter/DemoAdapter.kt
@@ -0,0 +1,23 @@
+package com.owulia.wowcool.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class DemoAdapter : RecyclerView.Adapter<DemoAdapterHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DemoAdapterHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: DemoAdapterHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class DemoAdapterHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
