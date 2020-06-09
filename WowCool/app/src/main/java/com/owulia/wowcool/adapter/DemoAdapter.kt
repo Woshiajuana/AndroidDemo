6158eb69992e15d4afc5597dd7642e39f5be1f74
Woshiajuana <zhigang.chen@owulia.com>
2020-06-09 21:37:03 +0800

    subcode code

--- WowCool/app/src/main/java/com/owulia/wowcool/adapter/DemoAdapter.kt
+++ WowCool/app/src/main/java/com/owulia/wowcool/adapter/DemoAdapter.kt
@@ -1,21 +1,24 @@
 package com.owulia.wowcool.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.owulia.wowcool.R
 
 class DemoAdapter : RecyclerView.Adapter<DemoAdapterHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DemoAdapterHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.demo_card_cell, parent, false)
+        return DemoAdapterHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return 10
     }
 
     override fun onBindViewHolder(holder: DemoAdapterHolder, position: Int) {
-        TODO("Not yet implemented")
+
     }
 
 }
--- WowCool/app/src/main/java/com/owulia/wowcool/fragment/DemoFragment.kt
+++ WowCool/app/src/main/java/com/owulia/wowcool/fragment/DemoFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.owulia.wowcool.R
+import com.owulia.wowcool.adapter.DemoAdapter
+import kotlinx.android.synthetic.main.fragment_demo.*
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +21,14 @@ class DemoFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_demo, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        rvDemoMain.apply {
+            layoutManager = GridLayoutManager(context, 3)
+            adapter = DemoAdapter()
+        }
     }
 
 }
