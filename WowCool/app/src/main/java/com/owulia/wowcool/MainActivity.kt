dffd5494b0d97379a3342280af3bd302547c424a
Woshiajuana <zhigang.chen@owulia.com>
2020-06-12 16:36:26 +0800

    subcode code

--- WowCool/app/src/main/java/com/owulia/wowcool/MainActivity.kt
+++ WowCool/app/src/main/java/com/owulia/wowcool/MainActivity.kt
@@ -2,24 +2,11 @@ package com.owulia.wowcool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.core.content.ContextCompat
-import androidx.navigation.Navigation
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import com.owulia.wowcool.fragment.HomeFragment
-import com.owulia.wowcool.fragment.DemoFragment
-import com.owulia.wowcool.fragment.MineFragment
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val navController = Navigation.findNavController(this, R.id.fragment)
-//        val configuration = AppBarConfiguration.Builder(navController.graph).build()
-//        NavigationUI.setupActionBarWithNavController(this, navController, configuration)
-//        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
