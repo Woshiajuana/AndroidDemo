0cdef1ad2bb3e881b66ae4e41cdf1d44b2f39c18
Woshiajuana <zhigang.chen@owulia.com>
2020-06-13 23:10:06 +0800

    subcode code

--- WowCool/app/src/main/java/com/owulia/wowcool/widget/WowSlideMenuView.kt
+++ WowCool/app/src/main/java/com/owulia/wowcool/widget/WowSlideMenuView.kt
@@ -2,17 +2,94 @@ package com.owulia.wowcool.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import java.lang.IndexOutOfBoundsException
 
 class WowSlideMenuView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ViewGroup(context, attrs, defStyleAttr) {
 
-    override fun onLayout(p0: Boolean, p1: Int, p2: Int, p3: Int, p4: Int) {
-        TODO("Not yet implemented")
+    private val tag = "WowSlideMenuView"
+
+    lateinit var mvContentWrap: WowSlideContentWrap
+    lateinit var mvMenuWrap: WowSlideMenuWrap
+
+    // 这里可以拿到自己的孩子
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        if (childCount != 2) {
+            throw IndexOutOfBoundsException("need two child")
+        }
+        Log.d(tag, "onFinishInflate 孩子多少个 => $childCount")
+        // 取出孩子
+        when (val childOne = getChildAt(0)) {
+            is WowSlideContentWrap ->  mvContentWrap = childOne
+            is WowSlideMenuWrap -> mvMenuWrap = childOne
+        }
+        when (val childTwo = getChildAt(1)) {
+            is WowSlideContentWrap ->  mvContentWrap = childTwo
+            is WowSlideMenuWrap -> mvMenuWrap = childTwo
+        }
     }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        // 父控件的大小
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+
+        // 测量第一个内容部分
+        // 宽度，跟父控件一样宽，高度有三种情况，如果指定大小，那么我们获取
+        // 到它的大小，直接测了
+        // 如果是包内容，at_most 如果是 match_parent，那就给它大小
+        val contentHeight = mvContentWrap.layoutParams.height
+        Log.d(tag, "contentHeight => $contentHeight")
+        val contentHeightMeasureSpec =
+            when (contentHeight) {
+                LayoutParams.MATCH_PARENT -> MeasureSpec.makeMeasureSpec(heightSize, MeasureSpec.EXACTLY)
+                LayoutParams.WRAP_CONTENT -> MeasureSpec.makeMeasureSpec(heightSize, MeasureSpec.AT_MOST)
+                else -> MeasureSpec.makeMeasureSpec(contentHeight, MeasureSpec.EXACTLY)
+            }
+        mvContentWrap.measure(widthMeasureSpec, contentHeightMeasureSpec)
+
+        // 拿到内容部分测量以后的高度
+        val contentMeasureHeight = mvContentWrap.measuredHeight
+        Log.d(tag, "contentMeasureHeight => $contentMeasureHeight")
+        val menuWidth = mvMenuWrap.layoutParams.width
+        Log.d(tag, "menuWidth => $contentHeight")
+        // 测量编辑部分的宽度: 3/4 高度跟内容高度一样
+        val menuWithMeasureSpec = MeasureSpec.makeMeasureSpec(menuWidth, MeasureSpec.AT_MOST)
+        mvMenuWrap.measure(menuWithMeasureSpec, MeasureSpec.makeMeasureSpec(contentMeasureHeight, MeasureSpec.EXACTLY))
+        Log.d(tag, "mvMenuWrap.measuredHeight => ${mvMenuWrap.measuredHeight}")
+        Log.d(tag, "mvMenuWrap.measuredWidth => ${mvMenuWrap.measuredWidth}")
+
+        // 测量自己
+        // 宽度就是前面的总和，高度和内容一样
+
+        setMeasuredDimension(widthSize + mvMenuWrap.measuredWidth, contentMeasureHeight)
+
+    }
+
+    override fun onLayout(p0: Boolean, p1: Int, p2: Int, p3: Int, p4: Int) {
+        // 拜访内容
+        val contentLeft = -100
+        val contentTop = 0
+        val contentRight = contentLeft + mvContentWrap.measuredWidth
+        val contentBottom = contentTop + mvContentWrap.measuredHeight
+        mvContentWrap.layout(contentLeft, contentTop, contentRight, contentBottom)
+
+        // 拜访操作内容
+        val menuLeft = contentRight
+        val menuTop = 0
+        val menuRight = menuLeft + mvMenuWrap.measuredWidth
+        val menuBottom = menuTop + mvMenuWrap.measuredHeight
+        mvMenuWrap.layout(menuLeft, menuTop, menuRight, menuBottom)
+
+
+    }
 }
 
 class WowSlideMenuWrap @JvmOverloads constructor(
@@ -24,3 +101,7 @@ class WowSlideMenuWrap @JvmOverloads constructor(
     }
 
 }
+
+class WowSlideContentWrap @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr)
