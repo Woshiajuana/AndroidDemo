dec70145dc88806a3c5ea3e7de2fd7c2cd7ad956
Woshiajuana <zhigang.chen@owulia.com>
2020-06-16 10:57:46 +0800

    subcode code

---
+++ TaoBaoUnion/app/src/main/java/com/owulia/taobaounion/ui/fragment/SearchFragment.kt
@@ -0,0 +1,14 @@
+package com.owulia.taobaounion.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.owulia.taobaounion.R
+import com.owulia.taobaounion.base.BaseFragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SearchFragment : BaseFragment() {
+
+    override fun getRootViewResId(): Int = R.layout.fragment_search
+
+}
