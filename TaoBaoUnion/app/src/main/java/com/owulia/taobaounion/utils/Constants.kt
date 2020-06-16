99570207939437bc7bbfe40fdbe811651dbca7cc
Woshiajuana <zhigang.chen@owulia.com>
2020-06-16 22:29:01 +0800

    subcode code

--- TaoBaoUnion/app/src/main/java/com/owulia/taobaounion/utils/Constants.kt
+++ TaoBaoUnion/app/src/main/java/com/owulia/taobaounion/utils/Constants.kt
@@ -3,7 +3,8 @@ package com.owulia.taobaounion.utils
 class Constants {
 
     companion object {
-        const val BASE_URL = "https://www.sunofbeach.net/shop/api/"
+//        const val BASE_URL = "https://www.sunofbeach.net/shop/api/"
+        const val BASE_URL = "https://api.sunofbeach.net/shop/"
     }
 
 }
