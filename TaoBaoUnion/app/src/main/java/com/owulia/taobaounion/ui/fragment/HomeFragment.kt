77950142dc254b9881d356d04f0b8c794d317e1b
Woshiajuana <zhigang.chen@owulia.com>
2020-06-16 10:54:43 +0800

    SUBCODE CODE

---
+++ TaoBaoUnion/app/src/main/java/com/owulia/taobaounion/ui/fragment/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.owulia.taobaounion.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.owulia.taobaounion.R
+import com.owulia.taobaounion.base.BaseFragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeFragment : BaseFragment() {
+
+    override fun getRootViewResId(): Int = R.layout.fragment_home
+
+
+}
