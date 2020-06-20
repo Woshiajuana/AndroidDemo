26b9c913811737cc5968c3e449f6c5d28972b359
Woshiajuana <zhigang.chen@owulia.com>
2020-06-20 20:32:52 +0800

    subcode code

--- TaoBaoUnion/app/src/main/java/com/owulia/taobaounion/ui/custom/LoadingView.kt
+++ TaoBaoUnion/app/src/main/java/com/owulia/taobaounion/ui/custom/LoadingView.kt
@@ -1,7 +1,9 @@
 package com.owulia.taobaounion.ui.custom
 
 import android.content.Context
+import android.graphics.Canvas
 import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 import com.owulia.taobaounion.R
 
@@ -9,8 +11,49 @@ class LoadingView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : AppCompatImageView(context, attrs, defStyleAttr) {
 
+
+    private var mNeedRotate = false
+    private var mDegrees = 0f
+
     init {
         setImageResource(R.mipmap.loading)
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        mNeedRotate = true
+        startRotate()
+    }
+
+    private fun startRotate () {
+        post(object : Runnable{
+            override fun run() {
+                mDegrees += 10f
+                if (mDegrees >= 360f) {
+                    mDegrees = 0f
+                }
+                invalidate()
+                if (visibility != View.VISIBLE && !mNeedRotate) {
+                    removeCallbacks(this)
+                } else {
+                    postDelayed(this, 10)
+                }
+            }
+        })
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        stopRotate()
+    }
+
+    private fun stopRotate () {
+        mNeedRotate = false
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        canvas?.rotate(mDegrees, width / 2f, height / 2f)
+        super.onDraw(canvas)
     }
 
 }
