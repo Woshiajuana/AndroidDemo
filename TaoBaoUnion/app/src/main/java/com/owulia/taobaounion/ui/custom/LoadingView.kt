48721753d22937040cc4548c35075c1210714b69
Woshiajuana <zhigang.chen@owulia.com>
2020-06-20 20:21:20 +0800

    subcode code

--- TaoBaoUnion/app/src/main/java/com/owulia/taobaounion/ui/custom/LoadingView.kt
+++ TaoBaoUnion/app/src/main/java/com/owulia/taobaounion/ui/custom/LoadingView.kt
@@ -1,4 +1,16 @@
 package com.owulia.taobaounion.ui.custom
 
-class LoadingView {
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+import com.owulia.taobaounion.R
+
+class LoadingView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr) {
+
+    init {
+        setImageResource(R.mipmap.loading)
+    }
+
 }
