03b396efbe26abd2115d14df191c47c52c6dc1b1
Woshiajuana <zhigang.chen@owulia.com>
2020-06-24 14:13:50 +0800

    subcode code

---
+++ WowCool/app/src/main/java/com/owulia/wowcool/base/BaseFragment.kt
@@ -0,0 +1,5 @@
+package com.owulia.wowcool.base
+
+class BaseFragment {
+
+}
---
+++ WowCool/app/src/main/java/com/owulia/wowcool/utils/WowToastUtils.kt
@@ -0,0 +1,37 @@
+package com.owulia.wowcool.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.widget.Toast
+import java.lang.Exception
+
+class WowToastUtils private constructor(
+    private val context: Context
+) {
+
+    companion object {
+
+        private var mContext: Context? = null
+        private var mToast: Toast? = null
+
+        fun init (context: Context) {
+            mContext = context
+        }
+
+        @SuppressLint("ShowToast")
+        fun show (text: String, context: Context? = null) {
+            val realContext = mContext?: context
+            if (realContext != null) {
+                if (mToast == null) {
+                    mToast =  Toast.makeText(realContext, text, Toast.LENGTH_SHORT)
+                } else {
+                    mToast?.setText(text)
+                }
+                mToast?.show()
+            } else {
+                throw Exception("need init or context")
+            }
+        }
+    }
+
+}
