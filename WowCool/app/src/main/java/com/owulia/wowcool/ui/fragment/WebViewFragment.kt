6fb61811685eacb2827d0696a875a53f74379f08
Woshiajuana <zhigang.chen@owulia.com>
2020-06-30 15:03:07 +0800

    subcode code

--- WowCool/app/src/main/java/com/owulia/wowcool/ui/fragment/WebViewFragment.kt
+++ WowCool/app/src/main/java/com/owulia/wowcool/ui/fragment/WebViewFragment.kt
@@ -1,11 +1,13 @@
 package com.owulia.wowcool.ui.fragment
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.view.View
-import android.webkit.WebViewClient
+import android.webkit.*
 import androidx.fragment.app.Fragment
 import com.owulia.wowcool.R
 import com.owulia.wowcool.base.BaseFragment
+import com.owulia.wowcool.utils.WowLogUtils
 import kotlinx.android.synthetic.main.fragment_web_view.*
 
 /**
@@ -20,10 +22,52 @@ class WebViewFragment : BaseFragment() {
         super.initView(view)
         vWebView.apply {
             settings.javaScriptEnabled = true
-            webViewClient = WebViewClient()
-            loadUrl("https://www.baidu.com")
-        }
+            webChromeClient = object : WebChromeClient() {
+                // 网页加载进度
+                override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                    vProgressBar.apply {
+                        progress = newProgress
+                        visibility = if (progress == 100) View.GONE else View.VISIBLE
+                    }
+                    super.onProgressChanged(view, newProgress)
+                    WowLogUtils.d(this, "onProgressChanged => ${newProgress}")
+                }
 
+                override fun onReceivedTitle(view: WebView?, title: String?) {
+                    super.onReceivedTitle(view, title)
+                    WowLogUtils.d(this, "onReceivedTitle => ${title}")
+                }
+            }
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    url: String?
+                ): Boolean {
+                    WowLogUtils.d(this, "shouldOverrideUrlLoading => ${url}")
+                    return false
+//                    return super.shouldOverrideUrlLoading(view, url)
+                }
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+                    WowLogUtils.d(this, "shouldOverrideUrlLoading => ${request?.url}")
+                    return false
+//                    return super.shouldOverrideUrlLoading(view, request)
+                }
+                // 开始加载
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    WowLogUtils.d(this, "onPageStarted => ${url}")
+                    super.onPageStarted(view, url, favicon)
+                }
+                // 加载完成
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    WowLogUtils.d(this, "onPageFinished => ${url}")
+                    super.onPageFinished(view, url)
+                }
+            }
+            loadUrl("http://www.baidu.com")
+        }
     }
 
 }
