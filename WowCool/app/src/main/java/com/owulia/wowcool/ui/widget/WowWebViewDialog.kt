64fa655e327e0327cdead2527c5d23e09bc6c310
Woshiajuana <zhigang.chen@owulia.com>
2020-07-02 13:55:01 +0800

    subcode code

--- WowCool/app/src/main/java/com/owulia/wowcool/ui/widget/WowWebViewDialog.kt
+++ WowCool/app/src/main/java/com/owulia/wowcool/ui/widget/WowWebViewDialog.kt
@@ -11,46 +11,62 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.owulia.wowcool.R
+import com.owulia.wowcool.utils.WowLinearSpacesItemDecorationHelper
 import kotlinx.android.synthetic.main.dialog_web_view.*
+import kotlinx.android.synthetic.main.item_dialog_web_view_cell.view.*
 
 class WowWebViewDialog(context: Context) : Dialog(context, R.style.WowDialog) {
+
+    private var vRootView: View? = null
+    var mWebExtendAdapter: WowWebViewDialogAdapter? = null
+    var mWebOperateAdapter: WowWebViewDialogAdapter? = null
 
     @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val view = LayoutInflater.from(context).inflate(R.layout.dialog_web_view, null)
-        setContentView(view)
-        view.layoutParams.apply {
+        vRootView = LayoutInflater.from(context).inflate(R.layout.dialog_web_view, null)
+        setContentView(vRootView!!)
+        initView()
+    }
+
+    private fun initView () {
+        // 设置弹窗宽度
+        vRootView?.layoutParams?.apply {
             width = context.resources.displayMetrics.widthPixels
         }
         window?.apply {
+            // 设置弹窗内容位置
             setGravity(Gravity.BOTTOM)
+            // 设置弹窗动画
             setWindowAnimations(R.style.Dialog_Bottom_Animation)
         }
+        // 设置 适配器
         vDialogWebExtend.apply {
             layoutManager = LinearLayoutManager(context).apply {
                 orientation = LinearLayoutManager.HORIZONTAL
             }
-            adapter = WowWebViewDialogAdapter()
-            addItemDecoration(object : RecyclerView.ItemDecoration() {
-
-            })
+            mWebExtendAdapter = WowWebViewDialogAdapter()
+            adapter = mWebExtendAdapter
+            addItemDecoration(WowLinearSpacesItemDecorationHelper(leftSpace = 20, rightSpace = 20, childCount = 10))
         }
         vDialogWebOperate.apply {
             layoutManager = LinearLayoutManager(context).apply {
                 orientation = LinearLayoutManager.HORIZONTAL
             }
-            adapter = WowWebViewDialogAdapter()
+            mWebOperateAdapter = WowWebViewDialogAdapter()
+            adapter = mWebOperateAdapter
+            addItemDecoration(WowLinearSpacesItemDecorationHelper(leftSpace = 20, rightSpace = 20, childCount = 10))
         }
     }
 
-    private fun initView () {
-
-    }
+    data class OperateItemBean (val icon: Int, val text: String)
 }
 
 class WowWebViewDialogAdapter : RecyclerView.Adapter<WowWebViewDialogAdapter.WowWebViewDialogViewHolder>() {
     class WowWebViewDialogViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    val mArrData = ArrayList<WowWebViewDialog.OperateItemBean>()
+    var setOnItemClickListener: ((View) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WowWebViewDialogViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_dialog_web_view_cell, parent, false)
@@ -58,10 +74,21 @@ class WowWebViewDialogAdapter : RecyclerView.Adapter<WowWebViewDialogAdapter.Wow
     }
 
     override fun getItemCount(): Int {
-        return 10
+        return mArrData.size
     }
 
     override fun onBindViewHolder(holder: WowWebViewDialogViewHolder, position: Int) {
-//        TODO("Not yet implemented")
+        val itemData = mArrData[position]
+        holder.itemView.apply {
+            setOnClickListener(setOnItemClickListener)
+            vItemText.text = itemData.text
+            vItemIcon.setImageResource(itemData.icon)
+        }
+    }
+
+    fun setData (data: List<WowWebViewDialog.OperateItemBean>) {
+        mArrData.clear()
+        mArrData.addAll(data)
+        notifyDataSetChanged()
     }
 }
--- WowCool/app/src/main/java/com/owulia/wowcool/utils/WowLinearSpacesItemDecorationHelper.kt
+++ WowCool/app/src/main/java/com/owulia/wowcool/utils/WowLinearSpacesItemDecorationHelper.kt
@@ -8,9 +8,9 @@ class WowLinearSpacesItemDecorationHelper (
     private val leftSpace: Int = 0,
     private val topSpace: Int = 0,
     private val rightSpace: Int = 0,
-    private val bottomSpace: Int = 0
+    private val bottomSpace: Int = 0,
+    private val childCount: Int = -1
 ) : RecyclerView.ItemDecoration() {
-
     override fun getItemOffsets(
         outRect: Rect,
         view: View,
@@ -21,23 +21,10 @@ class WowLinearSpacesItemDecorationHelper (
         // 判断位置
         val position = parent.getChildAdapterPosition(view) // item position
         outRect.apply {
-            left = leftSpace
+            left = if (childCount != -1 && position == 0) leftSpace * 2 else leftSpace
             top = topSpace
-            right = rightSpace
+            right = if (position == childCount - 1) rightSpace * 2 else rightSpace
             bottom = bottomSpace
         }
     }
-
-//    override fun getItemOffsets(
-//        outRect: Rect,
-//        view: View?,
-//        parent: RecyclerView?,
-//        state: RecyclerView.State?
-//    ) {
-//        super.getItemOffsets(outRect, view, parent!!, state!!)
-//        outRect.top = topSpace
-//        outRect.left = leftSpace
-//        outRect.right = rightSpace
-//        outRect.bottom = bottomSpace
-//    }
 }
