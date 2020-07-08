6e3e54da17ac241d8fdc8c1c0d92d962fa0abf1e
Woshiajuana <zhigang.chen@owulia.com>
2020-07-08 13:44:59 +0800

    subcode code

--- WowCool/app/src/main/java/com/owulia/wowcool/utils/AndroidBug5497Workaround.java
+++ WowCool/app/src/main/java/com/owulia/wowcool/utils/AndroidBug5497Workaround.java
@@ -2,64 +2,78 @@ package com.owulia.wowcool.utils;
 
 import android.app.Activity;
 import android.graphics.Rect;
+import android.os.Build;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
 
 /**
  * 解决WebView中输入时弹出软键盘，挡住输入框的问题
- * copy from https://www.jianshu.com/p/306482e17080
- * Created by ldw on 2018/5/23.
  */
-
 public class AndroidBug5497Workaround {
-
-    // For more information, see https://code.google.com/p/android/issues/detail?id=5497
-    // To use this class, simply invoke assistActivity() on an Activity that already has its content view set.
-
-    public static void assistActivity (Activity activity) {
+    public static void assistActivity(Activity activity) {
         new AndroidBug5497Workaround(activity);
     }
 
     private View mChildOfContent;
     private int usableHeightPrevious;
     private FrameLayout.LayoutParams frameLayoutParams;
+    private int contentHeight;
+    private boolean isfirst = true;
+    private Activity activity;
+    private int statusBarHeight;
 
-    private AndroidBug5497Workaround(final Activity activity) {
+    private AndroidBug5497Workaround(Activity activity) {
+        //获取状态栏的高度
+        int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        statusBarHeight = activity.getResources().getDimensionPixelSize(resourceId);
+        this.activity = activity;
         FrameLayout content = (FrameLayout) activity.findViewById(android.R.id.content);
         mChildOfContent = content.getChildAt(0);
+        //界面出现变动都会调用这个监听事件
         mChildOfContent.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             public void onGlobalLayout() {
-                possiblyResizeChildOfContent(activity);
+                if (isfirst) {
+                    contentHeight = mChildOfContent.getHeight();//兼容华为等机型
+                    isfirst = false;
+                }
+                possiblyResizeChildOfContent();
             }
         });
-//        mChildOfContent.getViewTreeObserver().removeOnGlobalLayoutListener();
-        frameLayoutParams = (FrameLayout.LayoutParams) mChildOfContent.getLayoutParams();
+        frameLayoutParams = (FrameLayout.LayoutParams)
+                mChildOfContent.getLayoutParams();
     }
 
-    private void possiblyResizeChildOfContent(Activity activity) {
+    //重新调整跟布局的高度
+    private void possiblyResizeChildOfContent() {
         int usableHeightNow = computeUsableHeight();
+        //当前可见高度和上一次可见高度不一致 布局变动
         if (usableHeightNow != usableHeightPrevious) {
-//            int usableHeightSansKeyboard = mChildOfContent.getRootView().getHeight();
-//            int usableHeightSansKeyboard = activity.getWindowManager().getDefaultDisplay().getHeight();;
-            int usableHeightSansKeyboard = activity.findViewById(android.R.id.content).getMeasuredHeight();
+            //int usableHeightSansKeyboard2 = mChildOfContent.getHeight();//兼容华为等机型
+            int usableHeightSansKeyboard = mChildOfContent.getRootView().getHeight();
             int heightDifference = usableHeightSansKeyboard - usableHeightNow;
-            if (heightDifference > (usableHeightSansKeyboard/4)) {
+            if (heightDifference > (usableHeightSansKeyboard / 4)) {
                 // keyboard probably just became visible
-                frameLayoutParams.height = usableHeightSansKeyboard - heightDifference;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    //frameLayoutParams.height = usableHeightSansKeyboard - heightDifference;
+                    frameLayoutParams.height = usableHeightSansKeyboard - heightDifference + statusBarHeight;
+                } else {
+                    frameLayoutParams.height = usableHeightSansKeyboard - heightDifference;
+                }
             } else {
-                // keyboard probably just became hidden
-                frameLayoutParams.height = usableHeightSansKeyboard;
+                frameLayoutParams.height = contentHeight;
             }
             mChildOfContent.requestLayout();
             usableHeightPrevious = usableHeightNow;
         }
     }
 
+    /**
+     * 计算mChildOfContent可见高度 ** @return
+     */
     private int computeUsableHeight() {
         Rect r = new Rect();
         mChildOfContent.getWindowVisibleDisplayFrame(r);
-        return (r.bottom - r.top);// 全屏模式下： return r.bottom
+        return (r.bottom - r.top);
     }
-
 }
---
+++ WowCool/app/src/main/java/com/owulia/wowcool/utils/AndroidBug5497Workaround1.java
@@ -0,0 +1,63 @@
+package com.owulia.wowcool.utils;
+
+import android.app.Activity;
+import android.graphics.Rect;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
+
+/**
+ * 解决WebView中输入时弹出软键盘，挡住输入框的问题
+ */
+
+public class AndroidBug5497Workaround1 {
+
+    // For more information, see https://code.google.com/p/android/issues/detail?id=5497
+    // To use this class, simply invoke assistActivity() on an Activity that already has its content view set.
+
+    public static void assistActivity (Activity activity) {
+        new AndroidBug5497Workaround1(activity);
+    }
+
+    private View mChildOfContent;
+    private int usableHeightPrevious;
+    private FrameLayout.LayoutParams frameLayoutParams;
+
+    private AndroidBug5497Workaround1(final Activity activity) {
+        FrameLayout content = (FrameLayout) activity.findViewById(android.R.id.content);
+        mChildOfContent = content.getChildAt(0);
+        mChildOfContent.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            public void onGlobalLayout() {
+                possiblyResizeChildOfContent(activity);
+            }
+        });
+//        mChildOfContent.getViewTreeObserver().removeOnGlobalLayoutListener();
+        frameLayoutParams = (FrameLayout.LayoutParams) mChildOfContent.getLayoutParams();
+    }
+
+    private void possiblyResizeChildOfContent(Activity activity) {
+        int usableHeightNow = computeUsableHeight();
+        if (usableHeightNow != usableHeightPrevious) {
+//            int usableHeightSansKeyboard = mChildOfContent.getRootView().getHeight();
+//            int usableHeightSansKeyboard = activity.getWindowManager().getDefaultDisplay().getHeight();;
+            int usableHeightSansKeyboard = activity.findViewById(android.R.id.content).getMeasuredHeight();
+            int heightDifference = usableHeightSansKeyboard - usableHeightNow;
+            if (heightDifference > (usableHeightSansKeyboard/4)) {
+                // keyboard probably just became visible
+                frameLayoutParams.height = usableHeightSansKeyboard - heightDifference;
+            } else {
+                // keyboard probably just became hidden
+                frameLayoutParams.height = usableHeightSansKeyboard;
+            }
+            mChildOfContent.requestLayout();
+            usableHeightPrevious = usableHeightNow;
+        }
+    }
+
+    private int computeUsableHeight() {
+        Rect r = new Rect();
+        mChildOfContent.getWindowVisibleDisplayFrame(r);
+        return (r.bottom - r.top);// 全屏模式下： return r.bottom
+    }
+
+}
