583a2e41cb92771e25e811f89d56323d5093f9ed
Woshiajuana <zhigang.chen@owulia.com>
2020-07-23 17:33:37 +0800

    subcode code

--- HeimaPlayer/app/src/main/java/com/owulia/heimaplayer/ui/activity/SplashActivity.kt
+++ HeimaPlayer/app/src/main/java/com/owulia/heimaplayer/ui/activity/SplashActivity.kt
@@ -1,12 +1,37 @@
 package com.owulia.heimaplayer.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.ViewPropertyAnimatorListener
 import com.owulia.heimaplayer.R
 import com.owulia.heimaplayer.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+import org.jetbrains.anko.startActivity
 
-class SplashActivity : BaseActivity() {
+class SplashActivity : BaseActivity(), ViewPropertyAnimatorListener {
 
     override fun getLayoutId(): Int = R.layout.activity_splash
+
+    override fun initData() {
+        super.initData()
+        ViewCompat.animate(imageView).apply {
+            scaleX(1.0f)
+            scaleY(1.0f)
+            duration = 2000
+            setListener(this@SplashActivity)
+        }
+    }
+
+    override fun onAnimationEnd(view: View?) {
+        // 动画结束
+        startActivity<MainActivity>()
+        finish()
+    }
+
+    override fun onAnimationCancel(view: View?) {
+    }
+
+    override fun onAnimationStart(view: View?) {
+    }
 
 }
