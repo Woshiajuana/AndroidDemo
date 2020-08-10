5e8aaff7ea288519ef14c9025830d7d4f724b6f6
Woshiajuana <zhigang.chen@owulia.com>
2020-08-10 21:47:47 +0800

    subcode

---
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/base/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.owulia.makekotlin.base
+
+class BaseActivity {
+
+}
---
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/widget/HeaderWidget.kt
@@ -0,0 +1,13 @@
+package com.owulia.makekotlin.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class HeaderWidget @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+
+
+}
