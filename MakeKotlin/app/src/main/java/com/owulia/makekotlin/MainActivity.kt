347ec8bba2d28e9620688b22b04e8d96fca519fb
Woshiajuana <zhigang.chen@owulia.com>
2020-08-10 17:46:02 +0800

    subcode

--- MakeKotlin/app/src/main/java/com/owulia/makekotlin/MainActivity.kt
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/MainActivity.kt
@@ -1,72 +1,12 @@
 package com.owulia.makekotlin
 
-import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import org.apache.weex.IWXRenderListener
-import org.apache.weex.WXSDKInstance
-import org.apache.weex.common.WXRenderStrategy
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity(), IWXRenderListener {
-    var mWXSDKInstance: WXSDKInstance? = null
-
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        setContentView(R.layout.activity_main)
-        mWXSDKInstance = WXSDKInstance(this)
-        mWXSDKInstance!!.registerRenderListener(this)
-        val pageName = "WXSample"
-//        String bundleUrl = "http://dotwe.org/raw/dist/38e202c16bdfefbdb88a8754f975454c.bundle.wx1";
-        //        String bundleUrl = "http://dotwe.org/raw/dist/38e202c16bdfefbdb88a8754f975454c.bundle.wx1";
-        val bundleUrl =
-            "https://ossmk2.jfpays.com/www_make_v1/app/0.0.36/mk_about.js"
-        mWXSDKInstance!!.renderByUrl(pageName, bundleUrl, null, null, WXRenderStrategy.APPEND_ASYNC)
-
-
     }
-
-    override fun onRefreshSuccess(instance: WXSDKInstance?, width: Int, height: Int) {
-    }
-
-    override fun onException(instance: WXSDKInstance?, errCode: String?, msg: String?) {
-    }
-
-    override fun onViewCreated(instance: WXSDKInstance?, view: View?) {
-        setContentView(view)
-    }
-
-    override fun onRenderSuccess(instance: WXSDKInstance?, width: Int, height: Int) {
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if (mWXSDKInstance != null) {
-            mWXSDKInstance?.onActivityResume()
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if (mWXSDKInstance != null) {
-            mWXSDKInstance?.onActivityPause()
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        if (mWXSDKInstance != null) {
-            mWXSDKInstance?.onActivityStop()
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        if (mWXSDKInstance != null) {
-            mWXSDKInstance?.onActivityDestroy()
-        }
-    }
-
 }
