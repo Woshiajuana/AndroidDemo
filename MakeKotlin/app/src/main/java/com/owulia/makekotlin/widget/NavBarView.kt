23ae4c74e176bac347902b42c4c45629590be968
Woshiajuana <zhigang.chen@owulia.com>
2020-08-16 09:56:35 +0800

    subcode

--- MakeKotlin/app/src/main/java/com/owulia/makekotlin/widget/NavBarView.kt
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/widget/NavBarView.kt
@@ -2,12 +2,16 @@ package com.owulia.makekotlin.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.FrameLayout
+import com.owulia.makekotlin.R
 
-class HeaderWidget @JvmOverloads constructor(
+class NavBarView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
-
+    init {
+        LayoutInflater.from(context).inflate(R.layout.widget_header, this, true)
+    }
 
 }
