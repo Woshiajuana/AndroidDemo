a066e267a7570ff8d2c1da7d451d8335634102fe
Woshiajuana <zhigang.chen@owulia.com>
2020-09-16 15:06:53 +0800

    subcode

---
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/model/GuidePageModel.kt
@@ -0,0 +1,6 @@
+package com.owulia.makekotlin.model
+
+data class GuidePageModel(
+    val bannerIcon: Int,
+    val textIcon: Int
+)
--- MakeKotlin/app/src/main/java/com/owulia/makekotlin/ui/activity/GuidePageActivity.kt
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/ui/activity/GuidePageActivity.kt
@@ -6,6 +6,8 @@ import com.owulia.makekotlin.R
 
 class GuidePageActivity : AppCompatActivity() {
 
+//    val
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guide_page)
