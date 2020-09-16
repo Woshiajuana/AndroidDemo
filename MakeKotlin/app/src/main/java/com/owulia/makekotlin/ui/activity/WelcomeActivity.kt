3419ec4a4a9e96dc3c1d419af0c13da0b5e42c62
Woshiajuana <zhigang.chen@owulia.com>
2020-09-16 10:16:20 +0800

    subcode

---
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/ui/activity/WelcomeActivity.kt
@@ -0,0 +1,20 @@
+package com.owulia.makekotlin.ui.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.owulia.makekotlin.R
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.anim_fade_in, R.anim.anim_fade_out)
+            finish()
+        }, 1200)
+    }
+}
