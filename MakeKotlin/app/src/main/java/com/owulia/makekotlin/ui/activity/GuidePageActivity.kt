e38da9bf97653c0894b849963499815cfb142f01
Woshiajuana <zhigang.chen@owulia.com>
2020-09-16 11:13:56 +0800

    subcode

---
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/ui/activity/GuidePageActivity.kt
@@ -0,0 +1,14 @@
+package com.owulia.makekotlin.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.owulia.makekotlin.R
+
+class GuidePageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guide_page)
+    }
+
+}
