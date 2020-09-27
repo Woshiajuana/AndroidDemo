099e7dfeaa8f7a105cef1132c10c871b16ec5e78
Woshiajuana <zhigang.chen@owulia.com>
2020-09-27 22:22:13 +0800

    subcode

--- MakeKotlin/app/src/main/java/com/owulia/makekotlin/bean/Api.kt
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/bean/Api.kt
@@ -2,6 +2,7 @@ package com.owulia.makekotlin.bean
 
 import com.owulia.makekotlin.utils.Constants
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface Api {
@@ -10,5 +11,5 @@ interface Api {
      * 注册账号检查服务
      * */
     @POST(Constants.DO_CHECK_ACCOUNT)
-    fun doCheckAccount () : Call<CheckAccountBean>
+    fun doCheckAccount (@Body params: Map<String, String>) : Call<CheckAccountBean>
 }
