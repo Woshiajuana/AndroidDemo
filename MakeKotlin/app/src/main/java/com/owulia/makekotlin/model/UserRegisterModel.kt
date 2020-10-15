8e614bdeaaefd1f63a66291f63d8ce2da1646521
Woshiajuana <zhigang.chen@owulia.com>
2020-10-15 16:34:14 +0800

    subcode

--- MakeKotlin/app/src/main/java/com/owulia/makekotlin/model/UserRegisterModel.kt
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/model/UserRegisterModel.kt
@@ -10,8 +10,18 @@ class UserRegisterModel : UserRegisterContacts.IModel {
     override fun doUserRegister(account: String, password: String, smsCode: String): Call<RespBean<Any>> {
         val params = HashMap<String, Any?>()
         params["loginNo"] = account
-        params["password"] = password
-        params["smsType"] = "COMMON"
+        params["loginPassword"] = password
+        params["smsCode"] = smsCode
+        params["browser"] = "browser"
+        params["dataSourceType"] = "NORMAL"
+        params["imei"] = "imei"
+        params["lbs"] = "12.21"
+        params["loginChannel"] = "APP"
+        params["loginIp"] = "127.0.0.1"
+        params["loginLocation"] = "12.21"
+        params["loginType"] = "PWD"
+        params["oem"] = "oem"
+        params["os"] = "ANDROID"
         return RetrofitManager.instant.getApi().doUserRegister(params)
     }
 
