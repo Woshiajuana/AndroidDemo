c4d4c5e8acc013bb239bfcdadbac0418bd6fcd49
Woshiajuana <zhigang.chen@owulia.com>
2020-10-15 14:41:35 +0800

    subcode

--- MakeKotlin/app/src/main/java/com/owulia/makekotlin/bean/RespBean.kt
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/bean/RespBean.kt
@@ -1,7 +1,13 @@
 package com.owulia.makekotlin.bean
 
+import com.owulia.makekotlin.utils.Constants
+
 data class RespBean<T : Any>(
     val code: Int?,
     val `data`: T?,
     val msg: String?
-)
+) {
+    fun isOk () : Boolean {
+        return code == Constants.CODE_SUCCESS
+    }
+}
---
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/utils/WowExceptionHandle.kt
@@ -0,0 +1,16 @@
+package com.owulia.makekotlin.utils
+
+class WowExceptionHandle {
+
+//    companion object {
+//
+//        fun handleException () : ResponseThrowable {
+//
+//        }
+//
+//    }
+//    fun handleException () : ResponseThrowable {
+//
+//    }
+
+}
