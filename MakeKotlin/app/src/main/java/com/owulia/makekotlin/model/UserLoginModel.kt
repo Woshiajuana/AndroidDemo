a735fc1fc6893ca01eae524bdfb45cc887c28141
Woshiajuana <zhigang.chen@owulia.com>
2020-10-16 10:49:47 +0800

    subcode

--- MakeKotlin/app/src/main/java/com/owulia/makekotlin/model/UserLoginModel.kt
+++ MakeKotlin/app/src/main/java/com/owulia/makekotlin/model/UserLoginModel.kt
@@ -10,8 +10,22 @@ class UserLoginModel : UserLoginContacts.IModel {
 
     override fun doUserLogin(account: String, password: String): Call<RespBean<RespUserInfoBean>> {
         val params = HashMap<String, Any>()
-        params["account"] = account
-        params["password"] = password
+        params["username"] = account
+        params["loginPassword"] = password
+        params["code"] = "mf37"
+        params["grant_type"] = "password"
+        params["scope"] = "server"
+        params["browser"] = "browser"
+        params["imei"] = "862573030733839"
+        params["lbs"] = "0.0,0.0"
+        params["loginChannel"] = "APP"
+        params["loginLocation"] = "0.0,0.0"
+        params["loginType"] = "PWD"
+        params["oem"] = "oem"
+        params["loginIp"] = "127.0.0.1"
+        params["dataSourceType"] = "NORMAL"
+        params["userKind"] = "OPERATOR"
+        params["os"] = "ANDROID"
         return RetrofitManager.instant.getApi().doUserLogin(params)
     }
 
