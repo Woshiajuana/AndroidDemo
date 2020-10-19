83375ed15b673ec7b49d350e037b17acc9290b42
Woshiajuana <zhigang.chen@owulia.com>
2020-10-19 16:36:44 +0800

    subcode

--- MVVMDemo/app/src/main/java/com/owulia/mvvmdemo/MainActivity.kt
+++ MVVMDemo/app/src/main/java/com/owulia/mvvmdemo/MainActivity.kt
@@ -2,11 +2,16 @@ package com.owulia.mvvmdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.owulia.mvvmdemo.viewmodel.OnSellViewModel
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel = ViewModelProvider(this).get(OnSellViewModel::class.java)
+        viewModel.loadContent()
     }
 }
--- MVVMDemo/app/src/main/java/com/owulia/mvvmdemo/viewmodel/OnSellViewModel.kt
+++ MVVMDemo/app/src/main/java/com/owulia/mvvmdemo/viewmodel/OnSellViewModel.kt
@@ -2,7 +2,9 @@ package com.owulia.mvvmdemo.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.owulia.mvvmdemo.repository.OnSellRepository
+import kotlinx.coroutines.launch
 
 class OnSellViewModel : ViewModel() {
 
@@ -10,20 +12,36 @@ class OnSellViewModel : ViewModel() {
         OnSellRepository()
     }
 
+    companion object {
+        // 默认第一页
+        const val DEFAULT_PAGE = 1
+    }
+
+    // 当前页
+    private val mCurrentPage = DEFAULT_PAGE
+
     /**
      * 加载更多内容
      * */
     fun loaderMore () {
-        
+
     }
 
     /**
      * 加载首页内容
      * */
     fun loadContent () {
-
+        listContentByPage(mCurrentPage)
     }
 
-    val contentList = MutableLiveData<MutableList<>>()
+    private fun listContentByPage (page: Int) {
+        println("result => 开始请求")
+        viewModelScope.launch {
+            val onSellList = onSellRepository.getOnSellList(page)
+            println("result => ${onSellList.tbk_dg_optimus_material_response.result_list.map_data.size}")
+        }
+    }
+
+    val contentList = MutableLiveData<MutableList<String>>()
 
 }
