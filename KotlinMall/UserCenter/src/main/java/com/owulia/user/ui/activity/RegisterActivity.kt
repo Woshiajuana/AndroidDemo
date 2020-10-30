cf6620d325ea7d8cb282426867e5ffeb10806d17
Woshiajuana <zhigang.chen@owulia.com>
2020-10-30 14:32:50 +0800

    subcode

--- KotlinMall/UserCenter/src/main/java/com/owulia/user/ui/activity/RegisterActivity.kt
+++ KotlinMall/UserCenter/src/main/java/com/owulia/user/ui/activity/RegisterActivity.kt
@@ -3,11 +3,17 @@ package com.owulia.user.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.owulia.user.R
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.toast
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        mRegisterBtn.setOnClickListener {
+            toast("xxx")
+        }
     }
 }
