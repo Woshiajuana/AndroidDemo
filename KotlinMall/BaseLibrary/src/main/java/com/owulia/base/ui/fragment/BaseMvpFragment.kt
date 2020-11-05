09296015ba64d4c29e61f412270ca3537beba59f
Woshiajuana <zhigang.chen@owulia.com>
2020-11-05 18:06:25 +0800

    subcode

---
+++ KotlinMall/BaseLibrary/src/main/java/com/owulia/base/ui/fragment/BaseMvpFragment.kt
@@ -0,0 +1,46 @@
+package com.owulia.base.ui.fragment
+
+import android.os.Bundle
+import com.owulia.base.common.BaseApplication
+import com.owulia.base.injection.component.ActivityComponent
+import com.owulia.base.injection.component.DaggerActivityComponent
+import com.owulia.base.injection.module.ActivityModule
+import com.owulia.base.injection.module.LifecycleProviderModule
+import com.owulia.base.presenter.BasePresenter
+import com.owulia.base.presenter.view.BaseView
+import javax.inject.Inject
+
+abstract class BaseMvpFragment<T : BasePresenter<*>> : BaseFragment(), BaseView {
+
+    override fun showLoading() {
+    }
+
+    override fun hideLoading() {
+    }
+
+    override fun onError() {
+    }
+
+    @Inject
+    lateinit var mPresenter: T
+
+    lateinit var activityComponent: ActivityComponent
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initActivityInjection()
+        injectComponent()
+    }
+
+    abstract fun injectComponent ()
+
+    private fun initActivityInjection() {
+        activityComponent = DaggerActivityComponent.builder()
+            .appComponent((activity!!.application as BaseApplication).appComponent)
+            .activityModule(ActivityModule(activity!!))
+            .lifecycleProviderModule(LifecycleProviderModule(this))
+            .build()
+    }
+
+}
