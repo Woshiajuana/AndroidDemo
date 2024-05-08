5ea4ca8181f67821c265e7b5763fadb1ea233fcf
chenzhigang <979703986@qq.com>
2024-05-08 22:54:13 +0800

    chore: 更新

--- Basic/app/src/main/java/com/daysnap/basic/MainActivity.java
+++ Basic/app/src/main/java/com/daysnap/basic/MainActivity.java
@@ -3,6 +3,10 @@ package com.daysnap.basic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogin = findViewById(R.id.btn_login);
+        EditText etUsername = findViewById(R.id.ed_username);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                Log.e("DDD => ", "输入的用户名称" + username);
+            }
+        });
     }
 }
