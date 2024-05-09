d908f940b0468311a9d04229b7dd4892ecc997cf
chenzhigang <979703986@qq.com>
2024-05-09 22:16:41 +0800

    chore: 测试

--- Basic/app/src/main/java/com/daysnap/basic/MainActivity.java
+++ Basic/app/src/main/java/com/daysnap/basic/MainActivity.java
@@ -1,7 +1,12 @@
 package com.daysnap.basic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +14,9 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationManager notificationManager;
+    private Notification notification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +33,31 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("DDD => ", "输入的用户名称" + username);
             }
         });
+
+        // 通知
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // 版本兼容
+        String channelId = "daysnap";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                channelId,
+                "测试通知",
+                NotificationManager.IMPORTANCE_HIGH
+            );
+        }
+
+        notification = new NotificationCompat.Builder(this, channelId)
+                .setContentTitle("官方通知") // 必须
+                .setSmallIcon(R.drawable.baseline_person_24) // 必须
+                .setContentText("通知内容通知内容通知内容通知内容")
+                .build();
+    }
+
+    public void sendNotification(View view){
+        notificationManager.notify(1, notification);
+    }
+
+    public void cancelNotification() {
+        //
     }
 }
