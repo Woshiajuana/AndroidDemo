f1489a0b8ff7c854d49694930a18f225425df44c
chenzhigang <979703986@qq.com>
2024-06-09 20:20:13 +0800

    chore: 更新

--- Basic/app/src/main/java/com/daysnap/basic/IntentActivity.java
+++ Basic/app/src/main/java/com/daysnap/basic/IntentActivity.java
@@ -1,13 +1,20 @@
 package com.daysnap.basic;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class IntentActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityResultLauncher<Intent> activityLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,21 @@ public class IntentActivity extends AppCompatActivity implements View.OnClickLis
         findViewById(R.id.btn_jump_sms).setOnClickListener(this);
         findViewById(R.id.btn_jump_my).setOnClickListener(this);
         findViewById(R.id.btn_send).setOnClickListener(this);
+        findViewById(R.id.btn_request).setOnClickListener(this);
+
+        activityLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK) {
+                    // 处理从启动的 Activity 返回的结果
+                    String data = result.getData().getStringExtra("result");
+                    // 在这里使用返回的数据
+                    TextView tv = findViewById(R.id.tv_return_result);
+                    tv.setText(data);
+                }
+            }
+        });
+
     }
 
     @Override
@@ -36,24 +58,36 @@ public class IntentActivity extends AppCompatActivity implements View.OnClickLis
             intent.setData(uri);
             startActivity(intent);
         } else if (id == R.id.btn_jump_sms) {
-            // 跳转到短信页面
+            // 跳转到短信页面 隐方式跳转
             Intent intent = new Intent();
             intent.setAction(Intent.ACTION_SENDTO);
             Uri uri = Uri.parse("smsto:12345");
             intent.setData(uri);
             startActivity(intent);
         } else if (id == R.id.btn_jump_my) {
+            // 跳转到自定义页面 隐方式跳转
             Intent intent = new Intent();
             intent.setAction("android.intent.action.VIEWPAGER");
             intent.addCategory(Intent.CATEGORY_DEFAULT);
             startActivity(intent);
         } else if (id == R.id.btn_send) {
+            // 跳转传递参数
             Intent intent = new Intent(this, IntentParamsActivity.class);
+//            intent.putExtra("name", "张三");
             // 创建包裹
             Bundle bundle = new Bundle();
             bundle.putString("name", "张三");
             intent.putExtras(bundle);
             startActivity(intent);
+        } else if (id == R.id.btn_request) {
+            // 跳转并接收返回结果
+            Intent intent = new Intent(this, IntentParamsActivity.class);
+            // 创建包裹
+            Bundle bundle = new Bundle();
+            bundle.putString("name", "张三");
+            intent.putExtras(bundle);
+//            startActivityForResult(intent, 1); // 这种写法过期了
+            activityLauncher.launch(intent);
         }
     }
 }
--- Basic/app/src/main/java/com/daysnap/basic/IntentParamsActivity.java
+++ Basic/app/src/main/java/com/daysnap/basic/IntentParamsActivity.java
@@ -2,7 +2,10 @@ package com.daysnap.basic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class IntentParamsActivity extends AppCompatActivity {
@@ -16,5 +19,19 @@ public class IntentParamsActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         String name = bundle.getString("name");
         tv.setText(name);
+
+        Button btn =  findViewById(R.id.btn_response);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnIntent = new Intent();
+                // 添加你想要返回的数据，使用putExtra方法
+                returnIntent.putExtra("result", "我是张三，你好呀！");
+                // 设置返回的结果码和数据
+                setResult(RESULT_OK, returnIntent);
+                // 结束这个Activity
+                finish();
+            }
+        });
     }
 }
