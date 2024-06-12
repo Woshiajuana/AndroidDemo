5b652b7d2db0106a72eb2eb179157fabe3cf6974
chenzhigang <979703986@qq.com>
2024-06-12 22:42:16 +0800

    chore: apk 安装

--- Basic/app/src/main/java/com/daysnap/basic/ProviderApkActivity.java
+++ Basic/app/src/main/java/com/daysnap/basic/ProviderApkActivity.java
@@ -1,9 +1,22 @@
 package com.daysnap.basic;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import android.view.View;
+
+import com.daysnap.basic.utils.ToastUtil;
+
+import java.io.File;
 
 public class ProviderApkActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,7 +32,52 @@ public class ProviderApkActivity extends AppCompatActivity implements View.OnCli
     public void onClick(View v) {
         int id = v.getId();
         if (id == R.id.btn_install) {
-            // 安装 apk
+            // 11 之后获取 MANAGE_EXTERNAL_STORAGE 权限
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                checkAndInstall();
+            } else {
+                // 还需要 存储相关 权限
+                // 安装 apk
+                installApk();
+            }
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    private void checkAndInstall() {
+        if (!Environment.isExternalStorageManager()) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setData(Uri.fromParts("package", getPackageName(), null));
+            startActivity(intent);
+        } else {
+            // 安装 apk
+            installApk();
+        }
+    }
+
+    private void installApk() {
+        String apkPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/xxx.apk";
+        // 获取应用包管理器
+        PackageManager pm = getPackageManager();
+        // 获取 apk 文件包信息
+        PackageInfo pi = pm.getPackageArchiveInfo(apkPath, PackageManager.GET_ACTIVITIES);
+        if (pi == null) {
+            // 安装文件已损坏
+            ToastUtil.show(this, "安装文件已损坏");
+            return;
+        }
+        Uri uri = Uri.parse(apkPath);
+        // 兼容7.0之后的写法
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            // 通过FileProvider 获得文件的Uri访问方式
+            uri = FileProvider.getUriForFile(this, getString(R.string.file_provider), new File(apkPath));
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        // 设置 Uri 的数据类型为 APK 文件
+        intent.setDataAndType(uri, "application/vnd.android.package-archive");
+        startActivity(intent);
     }
 }
