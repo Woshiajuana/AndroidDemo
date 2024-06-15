8e6c884b6249f20abb392bc259819d99f25375b4
chenzhigang <979703986@qq.com>
2024-06-15 22:34:49 +0800

    chore: lifecycle

---
+++ JetpckStudy/app/src/main/java/com/daysnap/jetpckstudy/ProcessLifecycleOwnerActivity.java
@@ -0,0 +1,14 @@
+package com.daysnap.jetpckstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProcessLifecycleOwnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_process_lifecycle_owner);
+    }
+}
