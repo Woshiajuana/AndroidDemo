f87f6283cd4956f4d885e9339e95d26f75a1de7e
chenzhigang <979703986@qq.com>
2024-07-21 13:56:19 +0800

    feat: number input

--- CustomView/app/src/main/java/com/ohshell/customview/activity/NumberInputActivity.java
+++ CustomView/app/src/main/java/com/ohshell/customview/activity/NumberInputActivity.java
@@ -3,14 +3,23 @@ package com.ohshell.customview.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.ohshell.customview.R;
+import com.ohshell.customview.components.NumberInputView;
 
 public class NumberInputActivity extends AppCompatActivity {
+
+    private String TAG = "NumberInputActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number_input);
+
+        NumberInputView niNumber = findViewById(R.id.ni_number);
+        niNumber.setOnValueChangeListener(value -> {
+            Log.d(TAG, "value => " + value);
+        });
     }
 }
--- CustomView/app/src/main/java/com/ohshell/customview/components/NumberInputView.java
+++ CustomView/app/src/main/java/com/ohshell/customview/components/NumberInputView.java
@@ -1,6 +1,7 @@
 package com.ohshell.customview.components;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,13 @@ public class NumberInputView extends RelativeLayout {
     private View btnMinus;
     private View btnPlus;
     private EditText etValue;
+    private OnValueChangeListener onValueChangeListener;
+
+    private int min;
+    private int max;
+    private int defaultValue;
+    private int step;
+    private boolean disabled;
 
     public NumberInputView(Context context) {
         this(context, null);
@@ -26,6 +34,8 @@ public class NumberInputView extends RelativeLayout {
 
     public NumberInputView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        // 初始化属性
+        initAttrs(context, attrs);
 
         // 初始化 view
         initView(context);
@@ -33,21 +43,42 @@ public class NumberInputView extends RelativeLayout {
         // 初始化事件
         initEvent();
     }
+
+    private void initAttrs(Context context, AttributeSet attrs) {
+        TypedArray a =  context.obtainStyledAttributes(attrs, R.styleable.NumberInputView);
+        min = a.getInt(R.styleable.NumberInputView_min, Integer.MIN_VALUE);
+        max = a.getInt(R.styleable.NumberInputView_max, Integer.MAX_VALUE);
+        defaultValue = a.getInt(R.styleable.NumberInputView_defaultValue, 0);
+        step = a.getInt(R.styleable.NumberInputView_step, 1);
+        disabled = a.getBoolean(R.styleable.NumberInputView_disabled, false);
+        a.recycle();
+    }
+
     private void initView(Context context) {
         LayoutInflater.from(context).inflate(R.layout.view_number_input, this, true);
 
         btnMinus = findViewById(R.id.btn_minus);
         btnPlus = findViewById(R.id.btn_plus);
         etValue = findViewById(R.id.et_value);
+
+        setValue(defaultValue);
     }
 
     private void initEvent() {
         btnMinus.setOnClickListener(v -> {
-            setValue(value - 1);
+            int val = value - step;
+            if (val < min) {
+                val = min;
+            }
+            setValue(val);
         });
 
         btnPlus.setOnClickListener(v -> {
-            setValue(value + 1);
+            int val = value + step;
+            if (val > max) {
+                val = max;
+            }
+            setValue(val);
         });
     }
 
@@ -59,6 +90,65 @@ public class NumberInputView extends RelativeLayout {
     public void setValue(int value) {
         this.value = value;
         etValue.setText(String.valueOf(value));
+
+        setDisabled(disabled);
+
+        if (onValueChangeListener != null) {
+            onValueChangeListener.onChange(value);
+        }
     }
 
+    public void setOnValueChangeListener(OnValueChangeListener listener) {
+        this.onValueChangeListener = listener;
+    }
+    public interface OnValueChangeListener {
+        void onChange(int value);
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(int defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public void setStep(int step) {
+        this.step = step;
+    }
+
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+        if (disabled) {
+            btnMinus.setEnabled(false);
+            btnPlus.setEnabled(false);
+        } else {
+            btnMinus.setEnabled(value > min);
+            btnPlus.setEnabled(max > value);
+        }
+    }
 }
